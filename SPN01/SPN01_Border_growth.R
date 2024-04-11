3fda55b4d75f262e96e0b574e12d32f488553cb5
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2024-04-11 16:45:08 +0200

    Update SPN01 border growth

--- SPN01/SPN01_Border_growth.R
+++ SPN01/SPN01_Border_growth.R
@@ -13,10 +13,14 @@ sim$duplicate_internal_cells <- FALSE
 
 # Set the death activation level to avoid drift
 sim$death_activation_level <- 50
-
+# Set initial growth rates
+Clone1_gr <- 1
+Clone2_gr <- 2
+Clone3_gr <- 5
+Clone4_gr <- 15
 # Add first mutant
 sim$add_mutant(name = "Clone 1",
-               growth_rates = 1,
+               growth_rates = Clone1_gr,
                death_rates = 0.01)
 
 sim$place_cell("Clone 1", 500, 500)
@@ -27,7 +31,7 @@ clone1_tissue <-plot_tissue(sim)
 clone1_muller <- plot_muller(sim)
 
 # Add second mutant
-sim$add_mutant("Clone 2",growth_rates = 2,
+sim$add_mutant("Clone 2",growth_rates = Clone2_gr,
                death_rates = 0.01)
 sim$mutate_progeny(sim$choose_cell_in("Clone 1"), "Clone 2")
 sim$update_rates("Clone 1",rates = c(growth = 0, death=0))
@@ -35,66 +39,65 @@ sim$update_rates("Clone 1",rates = c(growth = 0, death=0))
 
 # Let the simulation evolve until "Clone 2" consists of 5000 cells
 sim$run_up_to_size("Clone 2", 100)
-sim$update_rates("Clone 1",rates = c(growth = 1, death=0))
+# sim$update_rates("Clone 1",rates = c(growth = Clone1_gr, death=0))
 sim$run_up_to_size("Clone 2",5000)
 
 
 clone2_tissue <-plot_tissue(sim)
 clone2_muller <- plot_muller(sim)
 ggsave("tissue/muller_02.pdf", clone2_muller,dpi=300, width = 8, height = 8)
-
+ggsave("tissue/tissue_02.pdf", clone2_tissue,dpi=300, width = 8, height = 8)
 # Add third mutant
-sim$add_mutant("Clone 3",growth_rates = 5,
+sim$add_mutant("Clone 3",growth_rates = Clone3_gr,
                death_rates = 0.01)
 sim$mutate_progeny(sim$choose_cell_in("Clone 2"), "Clone 3")
 sim$update_rates("Clone 2",rates = c(growth = 0, death=0))
 sim$run_up_to_size("Clone 3", 100)
-sim$update_rates("Clone 2",rates = c(growth = 2, death=0))
+# sim$update_rates("Clone 2",rates = c(growth = Clone2_gr, death=0))
 sim$run_up_to_size("Clone 3", 1e4)
 
 clone3_tissue <-plot_tissue(sim)
 clone3_muller <- plot_muller(sim)
 ggsave("tissue/muller_03.pdf", clone3_muller,dpi=300, width = 8, height = 8)
+ggsave("tissue/tissue_03.pdf", clone3_tissue,dpi=300, width = 8, height = 8)
+
 # Add last mutant
-sim$add_mutant("Clone 4",growth_rates = 15,
+sim$add_mutant("Clone 4",growth_rates = Clone4_gr,
                death_rates = 0.01)
 sim$mutate_progeny(sim$choose_cell_in("Clone 3"), "Clone 4")
 sim$update_rates("Clone 3",rates = c(growth = 0, death=0))
 sim$run_up_to_size("Clone 4", 100)
-sim$update_rates("Clone 3",rates = c(growth = 5, death=0.01))
+# sim$update_rates("Clone 3",rates = c(growth = Clone3_gr, death=0.01))
 
-sim$run_up_to_size("Clone 4", 3e5)
+sim$run_up_to_size("Clone 4", 4e5)
 
 clone4_tissue <-plot_tissue(sim)
 clone4_muller <- plot_muller(sim)
+clone4_state <- plot_state(sim)
 ggsave("tissue/muller_04.pdf", clone4_muller,dpi=300, width = 8, height = 8)
+ggsave("tissue/tissue_04.pdf", clone4_tissue,dpi=300, width = 8, height = 8)
 
 
 ## Sampling
-# Three boxes with 'ncells' cells each
-n_w <- n_h <- 50
-ncells <- 0.8*n_w*n_h
-bbox <- sim$search_sample(c("Clone 4" = ncells), n_w, n_h)
-
+# Define three boxes for sampling in
+# different position of the tissue
+# 
+bbox_1 <- new(TissueRectangle, c(145, 270), c(195, 320))
+bbox_2 <- new(TissueRectangle, c(250, 600), c(300, 650))
+bbox_3 <- new(TissueRectangle, c(600, 270), c(650, 320))
 plot_tissue(sim) +
-  geom_rect(xmin = bbox$lower_corner[1], xmax = bbox$upper_corner[1],
-            ymin = bbox$lower_corner[2], ymax = bbox$upper_corner[2],
-            fill = NA, color = "black")
-sim$sample_cells("A", bbox$lower_corner, bbox$upper_corner)
-plot_tissue(sim)
-bbox <- sim$search_sample(c("Clone 4" = ncells), n_w, n_h)
-plot_tissue(sim) +
-  geom_rect(xmin = bbox$lower_corner[1]+100, xmax = bbox$upper_corner[1]+100,
-            ymin = bbox$lower_corner[2]+100, ymax = bbox$upper_corner[2]+100,
-            fill = NA, color = "black")
-sim$sample_cells("B", bbox$lower_corner+100, bbox$upper_corner+100)
-plot_tissue(sim)
-bbox <- sim$search_sample(c("Clone 4" = ncells), n_w, n_h)
-plot_tissue(sim) +
-  geom_rect(xmin = bbox$lower_corner[1]+200, xmax = bbox$upper_corner[1]+200,
-            ymin = bbox$lower_corner[2]+200, ymax = bbox$upper_corner[2]+200,
-            fill = NA, color = "black")
-sim$sample_cells("C", bbox$lower_corner+200, bbox$upper_corner+200)
+  geom_rect(xmin = bbox_1$lower_corner[1], xmax = bbox_1$upper_corner[1],
+            ymin = bbox_1$lower_corner[2], ymax = bbox_1$upper_corner[2],
+            fill = NA, color = "red")+
+  geom_rect(xmin = bbox_2$lower_corner[1], xmax = bbox_2$upper_corner[1],
+            ymin = bbox_2$lower_corner[2], ymax = bbox_2$upper_corner[2],
+            fill = NA, color = "black") +
+  geom_rect(xmin = bbox_3$lower_corner[1], xmax = bbox_3$upper_corner[1],
+            ymin = bbox_3$lower_corner[2], ymax = bbox_3$upper_corner[2],
+            fill = NA, color = "blue")
+sim$sample_cells("A", bbox_1$lower_corner, bbox_1$upper_corner)
+sim$sample_cells("B", bbox_2$lower_corner, bbox_2$upper_corner)
+sim$sample_cells("C", bbox_3$lower_corner, bbox_3$upper_corner)
 # Get forest ####
 sampled_phylogeny <- sim$get_samples_forest()
 # Recap Plot ####
