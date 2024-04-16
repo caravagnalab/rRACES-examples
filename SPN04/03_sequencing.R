2c83ffbfc7ac29cd82f4b66bfdb66f235fe74aaa
Giovanni Santacatterina <gsantacatterina@login01.hpc.rd.areasciencepark.it>
2024-04-16 08:05:33 +0000

    working sequencing

--- SPN04/03_sequencing.R
+++ SPN04/03_sequencing.R
@@ -11,7 +11,11 @@ set.seed(seed)
 phylo_forest <- rRACES::load_phylogenetic_forest("data/phylo_forest.sff")
 
 # Simulate sequencing ####
-seq_results <- simulate_seq(phylo_forest, coverage = 80)
+chromosomes <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y")
+seq_results <- parallel::mclapply(chromosomes, function(c) {
+	simulate_seq(phylo_forest, coverage = 80, chromosomes = c, write_SAM = FALSE)
+}, mc.cores = parallel::detectCores()) %>% do.call("bind_rows", .)
+#seq_results <- simulate_seq(phylo_forest, coverage = 80)
 
 seq_to_long <- function(seq_results) {
   # get names of samples
