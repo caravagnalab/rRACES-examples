662f0de0ef5c61b4bdd73fa958942c9a336780e8
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2024-04-18 10:56:14 +0200

    Update SPN01 growth models

---
+++ SPN01/01_tissue_Homogeneous_growth.R
@@ -0,0 +1,98 @@
+rm(list = ls())
+library(rRACES)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+# Set directories
+dir.create(path = "tissue_homo", recursive = TRUE)
+# Prep simulation ####
+sim <- new(Simulation, seed = 5, save_snapshot = F)
+sim$update_tissue(1e3, 1e3)
+# Set the "border" growth model
+sim$duplicate_internal_cells <- TRUE
+
+# Set the death activation level to avoid drift
+sim$death_activation_level <- 50
+
+# Add first mutant
+sim$add_mutant(name = "Clone 1",
+               growth_rates = 1,
+               death_rates = 0.01)
+
+sim$place_cell("Clone 1", 500, 500)
+
+# Let the simulation evolve until "Clone 1" consists of 1300 cells
+sim$run_up_to_size("Clone 1", 1300)
+clone1_tissue <-plot_tissue(sim)
+clone1_muller <- plot_muller(sim)
+
+# Add second mutant
+sim$add_mutant("Clone 2",growth_rates = 2,
+               death_rates = 0.01)
+sim$mutate_progeny(sim$choose_cell_in("Clone 1"), "Clone 2")
+sim$update_rates("Clone 1",rates = c(growth = 0, death=0))
+
+
+# Let the simulation evolve until "Clone 2" consists of 5000 cells
+sim$run_up_to_size("Clone 2", 100)
+# sim$update_rates("Clone 1",rates = c(growth = 1, death=0))
+sim$run_up_to_size("Clone 2",5000)
+
+
+clone2_tissue <-plot_tissue(sim)
+clone2_muller <- plot_muller(sim)
+ggsave("tissue_homo/muller_02.pdf", clone2_muller,dpi=300, width = 8, height = 8)
+
+# Add third mutant
+sim$add_mutant("Clone 3",growth_rates = 5,
+               death_rates = 0.01)
+sim$mutate_progeny(sim$choose_cell_in("Clone 2"), "Clone 3")
+sim$update_rates("Clone 2",rates = c(growth = 0, death=0))
+sim$run_up_to_size("Clone 3", 100)
+# sim$update_rates("Clone 2",rates = c(growth = 2, death=0))
+sim$run_up_to_size("Clone 3", 1e4)
+
+clone3_tissue <-plot_tissue(sim)
+clone3_muller <- plot_muller(sim)
+ggsave("tissue_homo/muller_03.pdf", clone3_muller,dpi=300, width = 8, height = 8)
+# Add last mutant
+sim$add_mutant("Clone 4",growth_rates = 15,
+               death_rates = 0.01)
+sim$mutate_progeny(sim$choose_cell_in("Clone 3"), "Clone 4")
+sim$update_rates("Clone 3",rates = c(growth = 0, death=0))
+sim$run_up_to_size("Clone 4", 100)
+# sim$update_rates("Clone 3",rates = c(growth = 5, death=0.01))
+
+sim$run_up_to_size("Clone 4", 2e5)
+
+clone4_tissue <-plot_tissue(sim)
+clone4_muller <- plot_muller(sim)
+plot_state(sim)
+ggsave("tissue_homo/muller_04.pdf", clone4_muller,dpi=300, width = 8, height = 8)
+
+## Sampling
+# Three boxes with 'ncells' cells each
+n_w <- n_h <- 50
+ncells <- 0.8*n_w*n_h
+bbox <- sim$search_sample(c("Clone 4" = ncells), n_w, n_h)
+
+plot_tissue(sim) +
+  geom_rect(xmin = 501, xmax = 550,
+            ymin = 501, ymax = 550,
+            fill = NA, color = "black")
+sim$sample_cells("A", c(501,501), c(550,550))
+plot_tissue(sim)
+bbox <- sim$search_sample(c("Clone 4" = ncells), n_w, n_h)
+plot_tissue(sim) +
+  geom_rect(xmin = bbox$lower_corner[1], xmax = bbox$upper_corner[1],
+            ymin = bbox$lower_corner[2], ymax = bbox$upper_corner[2],
+            fill = NA, color = "black")
+sim$sample_cells("B", bbox$lower_corner, bbox$upper_corner)
+plot_tissue(sim)
+bbox <- sim$search_sample(c("Clone 4" = ncells), n_w, n_h)
+plot_tissue(sim) +
+  geom_rect(xmin = 700, xmax = 749,
+            ymin = 500, ymax = 549,
+            fill = NA, color = "black")
+sim$sample_cells("C", c(700,500), c(749,549))
+ggsave("tissue_homo/tissue_04.pdf", dpi=300, width = 8, height = 8)
---
+++ SPN01/02_mutations.R
@@ -0,0 +1,64 @@
+rm(list = ls())
+library(rRACES)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+# Set directories
+dir.create(path = "data", recursive = TRUE)
+
+# load the samples forest from "samples_forest.sff" and store it in `forest`
+forest <- load_samples_forest("samples_forest_border_growth.sff")
+# building a mutation engine by using the "GRCh38" set-up configuration
+m_engine <- build_mutation_engine(setup_code = "GRCh38")
+m_engine <- build_mutation_engine(setup_code = "GRCh38", context_sampling = 50)
+
+## Drivers for the tumors
+SNV_Clone1 = SNV("5", 112839942, "T") ## APC R1450*
+CNA_Clone2 = CNA(type = "D", "5",
+                 pos_in_chr = 67522671, len = 1e7,allele = 0)
+SNV_Clone3 = SNV("12", 25245350, "A")
+SNV_Clone4 = SNV("3",179218303, "A") # NC_000003.12:179218302:G:A
+
+# Mutation rates (passengers)
+mu_SNV = 1e-8
+mu_CNA = 1e-11
+
+m_engine$add_mutant(mutant_name = "Clone 1",
+                    passenger_rates = c(SNV = mu_SNV),
+                    driver_SNVs = c(SNV_Clone1),
+                    driver_CNAs = c())
+
+m_engine$add_mutant(mutant_name = "Clone 2",
+                    passenger_rates = c(CNA = mu_CNA),
+                    driver_SNVs = c(),
+                    driver_CNAs = c(CNA_Clone2))
+
+m_engine$add_mutant(mutant_name = "Clone 3",
+                    passenger_rates = c(SNV = mu_SNV),
+                    driver_SNVs = c(SNV_Clone3), 
+                    driver_CNAs = c())
+
+m_engine$add_mutant(mutant_name = "Clone 4",
+                    passenger_rates = c(SNV = mu_SNV),
+                    driver_SNVs = c(SNV_Clone4),
+                    driver_CNAs = c())
+# Mutational signatures
+m_engine$add_exposure(
+  time = 0,
+  coefficients = c(
+    SBS1 = 0.4,
+    SBS5 = 0.3,
+    SBS13 = 0.3)
+)
+
+phylo_forest <- m_engine$place_mutations(forest, num_of_preneoplatic_mutations = 1000)
+phylo_forest$save("data/phylo_forest.sff")
+
+chromosomes <- paste0(seq(1:22))
+chromosomes <- c(chromosomes,"X", "Y")
+
+seq_results <- parallel::mclapply(chromosomes, function(c) {
+        simulate_seq(phylo_forest, coverage = 80, chromosomes = c, write_SAM = FALSE)
+}, mc.cores = parallel::detectCores()) %>% do.call("bind_rows", .)
+
+saveRDS(object =seq_results ,file = paste0("data/m_engine_border_growth_allele0.rds"))
