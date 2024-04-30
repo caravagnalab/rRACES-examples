c257800d03389bea89c51f36f21b09a5b5d66c37
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2024-04-30 16:37:14 +0000

    mc

--- SPN01/02_mutations.R
+++ SPN01/02_mutations.R
@@ -53,3 +53,13 @@ m_engine$add_exposure(
 
 phylo_forest <- m_engine$place_mutations(forest, num_of_preneoplatic_mutations = 1000)
 phylo_forest$save("data/phylo_forest.sff")
+
+chromosomes <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y")
+
+
+seq_results <- parallel::mclapply(chromosomes, function(c) {
+	simulate_seq(phylo_forest, coverage = 80, chromosomes = c, write_SAM = FALSE)
+}, mc.cores = parallel::detectCores()) %>% do.call("bind_rows", .)
+
+saveRDS(object =seq_results ,file = paste0("data/sequencing_homogeneous_growth.rds"))
+
