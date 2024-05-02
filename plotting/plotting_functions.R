9ba10fc6e206fd4adee9f6d0e049329ce2b9c5b1
valerianilucrezia <valerianilucrezia11@gmail.com>
2024-05-02 11:18:59 +0200

    bug fix plotting

--- plotting/plotting_functions.R
+++ plotting/plotting_functions.R
@@ -23,18 +23,18 @@
 seq_to_long <- function(seq_results) {
   # Extract sample names from column names
   sample_names <- strsplit(colnames(seq_results)[grepl(".VAF", colnames(seq_results), fixed = TRUE)], ".VAF") %>% unlist()
-
+  
   # Process each sample separately to create a list of data frames
   seq_df <- lapply(sample_names, function(sn) {
     # Select relevant columns for the current sample
     cc <- c("chr", "chr_pos", "ref", "alt", "causes", "classes", colnames(seq_results)[grepl(paste0(sn, "."), colnames(seq_results), fixed = TRUE)])
-
+    
     # Rename columns and add sample_name column
     seq_results[, cc] %>%
       `colnames<-`(c("chr", "chr_pos", "ref", "alt", "causes", "classes", "occurences", "coverage", "VAF")) %>%
       dplyr::mutate(sample_name = sn)
   }) %>% do.call("bind_rows", .)
-
+  
   # Rename and reorder columns
   seq_df %>%
     dplyr::rename(chr = chr, from = chr_pos, DP = coverage, NV = occurences, ALT = alt) %>%
@@ -55,20 +55,20 @@ seq_to_long <- function(seq_results) {
 plot_DR_gw <- function(seq_res, sample, chromosomes = paste0(c(1:22, "X", "Y")), cuts = c(0.01, 0.99), n = 5000) {
   if (any(!chromosomes %in% paste0(c(1:22, "X", "Y"))))
     stop("Invalid chromosome passed as input")
-
+  
   data <- seq_to_long(seq_res) %>%
     dplyr::mutate(chr = factor(chr, levels = chromosomes)) %>%
     dplyr::filter(chr %in% chromosomes) %>%
     dplyr::filter(classes == "germinal") %>%
     dplyr::mutate(mut_id = paste(chr, from, to, sep = ":"))
-
+  
   normal_data <- data %>% dplyr::filter(sample_name == "normal_sample")
   if (!sample %in% unique(data$sample_name)) {
     stop(paste("Inserted 'sample' is not available, available samples are:",
                paste0(unique(data$sample_name), collapse = ", ")))
   }
   tumour_data <- data %>% dplyr::filter(sample_name == sample) %>% dplyr::filter(VAF <= max(cuts), VAF >= min(cuts)) 
-
+  
   d <- tumour_data %>%
     dplyr::left_join(normal_data, suffix = c(".tumour", ".normal"), by = "mut_id") %>%
     dplyr::mutate(DR = DP.tumour / DP.normal) %>%
@@ -77,18 +77,18 @@ plot_DR_gw <- function(seq_res, sample, chromosomes = paste0(c(1:22, "X", "Y")),
     dplyr::ungroup() %>%
     dplyr::arrange(chr.tumour, from.tumour) %>%
     dplyr::mutate(abs_pos = 1:n())
-
+  
   chr_limits <- d %>%
     dplyr::group_by(chr.tumour) %>%
     dplyr::filter(abs_pos == min(abs_pos)) %>%
     dplyr::pull(abs_pos)
   chr_limits <- c(chr_limits, max(d$abs_pos))
-
+  
   chr_means <- d %>%
     dplyr::group_by(chr.tumour) %>%
     dplyr::summarise(mean_pos = (max(abs_pos) + min(abs_pos)) / 2) %>%
     dplyr::pull(mean_pos)
-
+  
   d %>%
     ggplot2::ggplot(mapping = ggplot2::aes(x = abs_pos, y = DR)) +
     ggplot2::geom_vline(xintercept = chr_means, linetype = "dashed", alpha = 0.2) +
@@ -120,18 +120,18 @@ plot_DR_gw <- function(seq_res, sample, chromosomes = paste0(c(1:22, "X", "Y")),
 plot_BAF_gw <- function(seq_res, sample, chromosomes = paste0(c(1:22, "X", "Y")), cuts = c(0.01, 0.99), n = 5000) {
   if (any(!chromosomes %in% paste0(c(1:22, "X", "Y"))))
     stop("Invalid chromosome passed as input")
-
+  
   data <- seq_to_long(seq_res) %>%
     dplyr::mutate(chr = factor(chr, levels = chromosomes)) %>%
     dplyr::filter(chr %in% chromosomes) %>%
     dplyr::filter(classes == "germinal")
-
+  
   if (!sample %in% unique(data$sample_name)) {
     stop(paste("Inserted 'sample' is not available, available samples are:",
                paste0(unique(data$sample_name), collapse = ", ")))
   }
   tumour_data <- data %>% dplyr::filter(sample_name == sample)
-
+  
   d <- tumour_data %>%
     dplyr::group_by(chr) %>%
     dplyr::sample_n(n, replace = T) %>%
@@ -139,18 +139,18 @@ plot_BAF_gw <- function(seq_res, sample, chromosomes = paste0(c(1:22, "X", "Y"))
     dplyr::arrange(chr, from) %>%
     dplyr::mutate(abs_pos = 1:n()) %>%
     dplyr::filter(VAF <= max(cuts), VAF >= min(cuts))
-
+  
   chr_limits <- d %>%
     dplyr::group_by(chr) %>%
     dplyr::filter(abs_pos == min(abs_pos)) %>%
     dplyr::pull(abs_pos)
   chr_limits <- c(chr_limits, max(d$abs_pos))
-
+  
   chr_means <- d %>%
     dplyr::group_by(chr) %>%
     dplyr::summarise(mean_pos = (max(abs_pos) + min(abs_pos)) / 2) %>%
     dplyr::pull(mean_pos)
-
+  
   d %>%
     ggplot2::ggplot(mapping = ggplot2::aes(x = abs_pos, y = VAF)) +
     ggplot2::geom_vline(xintercept = chr_means, linetype = "dashed", alpha = 0.2) +
@@ -182,18 +182,18 @@ plot_BAF_gw <- function(seq_res, sample, chromosomes = paste0(c(1:22, "X", "Y"))
 plot_VAF_gw <- function(seq_res, sample, chromosomes = paste0(c(1:22, "X", "Y")), cuts = c(0.05, 0.99), n = 1000) {
   if (any(!chromosomes %in% paste0(c(1:22, "X", "Y"))))
     stop("Invalid chromosome passed as input")
-
+  
   data <- seq_to_long(seq_res) %>%
     dplyr::mutate(chr = factor(chr, levels = chromosomes)) %>%
     dplyr::filter(chr %in% chromosomes) %>%
     dplyr::filter(classes != "germinal")
-
+  
   if (!sample %in% unique(data$sample_name)) {
     stop(paste("Inserted 'sample' is not available, available samples are:",
                paste0(unique(data$sample_name), collapse = ", ")))
   }
   tumour_data <- data %>% dplyr::filter(sample_name == sample)
-
+  
   d <- tumour_data %>%
     dplyr::arrange(chr, from) %>%
     dplyr::group_by(chr) %>%
@@ -201,18 +201,18 @@ plot_VAF_gw <- function(seq_res, sample, chromosomes = paste0(c(1:22, "X", "Y"))
     dplyr::ungroup() %>%
     dplyr::mutate(abs_pos = 1:n()) %>%
     dplyr::filter(VAF <= max(cuts), VAF >= min(cuts))
-
+  
   chr_limits <- d %>%
     dplyr::group_by(chr) %>%
     dplyr::filter(abs_pos == min(abs_pos)) %>%
     dplyr::pull(abs_pos)
   chr_limits <- c(chr_limits, max(d$abs_pos))
-
+  
   chr_means <- d %>%
     dplyr::group_by(chr) %>%
     dplyr::summarise(mean_pos = (max(abs_pos) + min(abs_pos)) / 2) %>%
     dplyr::pull(mean_pos)
-
+  
   d %>%
     ggplot2::ggplot(mapping = ggplot2::aes(x = abs_pos, y = VAF)) +
     ggplot2::geom_vline(xintercept = chr_means, linetype = "dashed", alpha = 0.2) +
@@ -253,22 +253,22 @@ plot_histogram_vaf <- function(
     stop("Colour_by parameter can be either 'causes' or 'classes'")
   if (any(!chromosomes %in% paste0(c(1:22, "X", "Y"))))
     stop("Invalid chromosomes passed as input")
-
+  
   data <- seq_to_long(seq_res) %>%
     dplyr::mutate(chr = factor(chr, levels = chromosomes)) %>%
     dplyr::filter(chr %in% chromosomes) %>%
     dplyr::filter(classes != "germinal") %>%
     dplyr::filter(sample_name != "normal_sample") %>%
     dplyr::filter(VAF <= max(cuts), VAF >= min(cuts))
-
+  
   if (!is.null(samples)) {
     if (any(!samples %in% unique(data$sample_name)))
       stop("Invalid sample name in samples parameter")
     data <- data %>% dplyr::filter(sample_name %in% samples)
   }
-
+  
   data$col <- data[[colour_by]]
-
+  
   data %>%
     ggplot2::ggplot(mapping = ggplot2::aes(x = VAF, col = col, fill = col)) +
     ggplot2::geom_histogram(bins = 100, alpha = 0.8) +
@@ -301,29 +301,29 @@ plot_marginals <- function(seq_res, chromosome, colour_by = "causes", samples = 
     stop("Colour_by parameter can be either 'causes' or 'classes'")
   if (!chromosome %in% paste0(c(1:22, "X", "Y")))
     stop("Invalid chr passed as input")
-
+  
   data <- seq_to_long(seq_res) %>%
     dplyr::filter(chr == chromosome) %>%
     dplyr::filter(classes != "germinal") %>%
     dplyr::filter(sample_name != "normal_sample") %>%
     dplyr::filter(VAF <= max(cuts), VAF >= min(cuts))
-
+  
   if (!is.null(samples)) {
     if (any(!samples %in% unique(data$sample_name)))
       stop("Invalid sample name in samples parameter")
     data <- data %>% dplyr::filter(sample_name %in% samples)
   }
-
+  
   combinations <- utils::combn(unique(data$sample_name), m = 2)
-
+  
   lapply(1:ncol(combinations), function(i) {
     couple <- combinations[, i]
     d1 <- data %>% dplyr::filter(sample_name == couple[1]) %>% dplyr::mutate(mut_id = paste(chr, from, to, sep = ":"))
     d2 <- data %>% dplyr::filter(sample_name == couple[2]) %>% dplyr::mutate(mut_id = paste(chr, from, to, sep = ":"))
-
+    
     djoin <- dplyr::full_join(d1, d2, by = 'mut_id')
     djoin$col <- djoin[[paste0(colour_by, ".x")]]
-
+    
     djoin %>%
       ggplot2::ggplot(mapping = ggplot2::aes(x = VAF.x, y = VAF.y, col = col)) +
       ggplot2::geom_point(alpha = 0.7) +
@@ -334,4 +334,5 @@ plot_marginals <- function(seq_res, chromosome, colour_by = "causes", samples = 
       ggplot2::theme(legend.position = "bottom")
   })
 }
+
 
