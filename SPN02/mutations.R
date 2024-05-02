099bc19c572f7b619c73c6deab85959ac9ad233b
Virginia Gazziero <148044855+vvvirgy@users.noreply.github.com>
2024-05-02 11:08:24 +0200

    some changes to spn02

--- SPN02/mutations.R
+++ SPN02/mutations.R
@@ -11,9 +11,9 @@ library(rRACES)
 
 m_engine <- build_mutation_engine(setup_code = "GRCh38")
 
-m_engine$add_mutant(mutant_name = "Clone 1",
-                    passenger_rates = c(SNV = 3e-8, CNA = 1e-11),
-                    driver_SNVs = SNV("7", 10510210, "A")) 
+# chr7	140753332	140753332	T	A	SNV	CLLE	BRAF	BRAF V600E
+m_engine$add_mutant("Clone 1", c(SNV = 5e-9), c(SNV("7", 140753332, "A"))) 
+
 
 m_engine$add_mutant(mutant_name = "Clone 2", 
                     passenger_rates = c(SNV = 3e-8, CNA = 1e-11),
@@ -24,4 +24,5 @@ m_engine$add_mutant(mutant_name = "Clone 3",
                     driver_SNVs = SNV("2", 47799065, "A"))
 
 m_engine$add_exposure(c(SBS1 = 0.5,SBS5 = 0.5))
-m_engine$add_exposure(clone3_born, c(SBS1 = 0.3,SBS5 = 0.3, SBS6 = 0.4))
+m_engine$add_exposure(22, c(SBS1 = 0.3,SBS5 = 0.3, SBS6 = 0.4))
+
