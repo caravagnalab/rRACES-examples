13be9861da17559884dc56b243acc31e32a79afb
Virginia Gazziero <148044855+vvvirgy@users.noreply.github.com>
2024-05-08 16:41:05 +0200

    SPN02 mutations

--- SPN02/tissue_simulation.R
+++ SPN02/tissue_simulation.R
@@ -7,11 +7,11 @@ set.seed(seed)
 
 sim = new(Simulation, 
           seed = seed,
-          save_snapshot = T)
+          save_snapshot = F)
 sim$update_tissue(1e3, 1e3)
-sim$duplicate_internal_cells <- FALSE
+sim$duplicate_internal_cells <- TRUE
 sim$death_activation_level <- 50
-
+dir.create("SPN02/tissue", recursive = T)
 # adding first clone
 
 sim$add_mutant(name = "Clone 1", 
@@ -32,6 +32,9 @@ sim$update_rates("Clone 1", rates = c(growth = 0, death = 1))
 sim$run_up_to_size("Clone 2", 3000)
 
 plot_muller(sim)
+ggsave("SPN02/tissue/clone2_muller.pdf")
+plot_tissue(sim)
+ggsave("SPN02/tissue/clone2_tissue.pdf")
 
 # adding third clone
 
@@ -42,10 +45,12 @@ sim$add_mutant(name = "Clone 3",
 
 sim$mutate_progeny(sim$choose_cell_in("Clone 2"), "Clone 3")
 # sim$update_rates("Clone 1", rates = c(growth = 0, death = 3))
-sim$update_rates("Clone 2", rates = c(growth = 0, death = 2))
-sim$run_up_to_size("Clone 3", 12000)
+sim$update_rates("Clone 2", rates = c(growth = 0, death = 3.5))
+sim$run_up_to_size("Clone 3", 15000)
 plot_muller(sim)
-ggsave(filename = "muller_clone3.pdf", muller)
+ggsave(filename = "SPN02/tissue/clone3_muller.pdf")
+plot_tissue(sim)
+ggsave("SPN02/tissue/clone3_tissue.pdf")
 
 # sampling twice from population of clone 3
 
@@ -64,11 +69,14 @@ plot_tissue(sim) +
             ymin = box2_p[2], ymax = box2_q[2], 
             fill = NA, color = "black") 
 
+ggsave("SPN02/tissue/clone3_sampling.pdf")
+
 sim$sample_cells("A", box1_p, box1_q)
 sim$sample_cells("B", box2_p, box2_q)
 
 # Get forest ####
 sampled_phylogeny <- sim$get_samples_forest()
+sampled_phylogeny$save("SPN02/tissue/forest.sff")
 
 # Recap Plot ####
 final_sampled_tissue <-plot_tissue(sim)
@@ -94,5 +102,5 @@ summary_patchwork = patchwork::wrap_plots(
   guides = 'auto', design = layout
 )
 
-ggsave(filename = "../rRACES_results/phylogeny_SPN02_new_border_growth_model.png",
-  plot = plot_phylogeny, bg = "white", width = 8)
+ggsave(filename = "SPN02/tissue/recap_plot.pdf", summary_patchwork, width = 15)
+
