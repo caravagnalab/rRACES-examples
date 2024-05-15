68736c5a9415f6135a358dcbae0d4161c98f749e
Giovanni Santacatterina <65920542+jovoni@users.noreply.github.com>
2024-05-15 12:32:57 +0200

    Update SNP04

--- SPN04/01_tissue.R
+++ SPN04/01_tissue.R
@@ -7,43 +7,67 @@ source("utils.R")
 
 seed <- 777
 set.seed(seed)
-dt <- .1
 
 # Prep simulation ####
 sim <- new(Simulation, seed = seed, save_snapshot = F)
+sim$history_delta <- 1
 sim$update_tissue(1e3, 1e3)
-# Set the "border" growth model
-sim$duplicate_internal_cells <- TRUE
 
 # Set the death activation level to avoid drift
 sim$death_activation_level <- 50
 
 # First and Second mutant ####
-sim$add_mutant(name = "Clone 1", growth_rates = .05, death_rates = .001)
+sim$add_mutant(name = "Clone 1", growth_rates = .1, death_rates = .01)
+
+####### CLONE 1 #######
 
 sim$place_cell("Clone 1", 500, 500)
-# Let the simulation evolve until "A+" consists of 1000 cells
-#sim$run_up_to_size("Clone 1", 1) # to observe muller from beginning
-sim <- run_up_to_size_by_steps(sim, "Clone 1", 1000, dt)
+sim$run_up_to_size("Clone 1", 1000)
+plot_muller(sim)
 
-sim$add_mutant("Clone 2",growth_rates = .1, death_rates = .001)
+####### CLONE 2 #######
+
+sim$add_mutant("Clone 2",growth_rates = .3, death_rates = .01)
+sim$update_rates("Clone 1", rates = c(growth = .033))
+
 sim$mutate_progeny(sim$choose_cell_in("Clone 1"), "Clone 2")
-sim$get_species()
-sim <- sweep_population(sim, "Clone 1", "Clone 2", .5, first_reduction = 10, delta_time = dt)
+sim$run_up_to_size("Clone 2", 2000)
 
-# Third mutant ####
-sim$add_mutant("Clone 3",growth_rates = .2, death_rates = .001)
+sim$update_rates("Clone 1", rates = c(growth = 0, death = .025))
+sim$run_up_to_size("Clone 2", 4000)
+
+sim$update_rates("Clone 1", rates = c(growth = 0, death = .05))
+sim$run_up_to_size("Clone 2", 8000)
+
+plot_muller(sim)
+
+####### CLONE 3 #######
+
+sim$add_mutant("Clone 3",growth_rates = 1, death_rates = .01)
+#sim$update_rates("Clone 2", rates = c(growth = .1, death = .05))
+sim$update_rates("Clone 2", rates = c(growth = .1))
+
 sim$mutate_progeny(sim$choose_cell_in("Clone 2"), "Clone 3")
+sim$run_up_to_size("Clone 3", 8000)
 
-sim <- sweep_population(sim, "Clone 2", "Clone 3", .75, first_reduction = 10, delta_time = dt)
-sim <- run_up_to_size_by_steps(sim, "Clone 3", .75e5, dt)
+sim$update_rates("Clone 2", rates = c(growth = 0, death = .025))
+sim$run_up_to_size("Clone 3", 16000)
+
+sim$update_rates("Clone 2", rates = c(growth = 0, death = .05))
+sim$run_up_to_size("Clone 3", 50000)
+
+sim$update_rates("Clone 1", rates = c(growth = 0, death = 100))
+sim$update_rates("Clone 2", rates = c(growth = 0, death = 100))
+
+sim$run_up_to_size("Clone 3", 70000)
 
 plot_tissue(sim)
 ggsave("tissue/tissue_00.pdf", dpi=300, width = 8, height = 8)
 plot_muller(sim)
 ggsave("tissue/muller_00.pdf", dpi=300, width = 8, height = 8)
 
-# First sampling ####
+
+####### SAMPLE A #######
 n_w <- n_h <- 15
 ncells <- .99*n_w*n_h
 bbox <- sim$search_sample(c("Clone 3" = ncells), n_w, n_h)
@@ -54,12 +78,17 @@ ggsave("tissue/tissue_01.pdf", dpi=300, width = 8, height = 8)
 plot_muller(sim)
 ggsave("tissue/muller_01.pdf", dpi=300, width = 8, height = 8)
 
-# Treatment ####
+####### TREATMENT #######
 treatment_start <- sim$get_clock()
-sim$update_rates("Clone 1",rates = c(growth = 0, death=.5))
-sim$update_rates("Clone 2",rates = c(growth = 0, death=.5))
-sim$update_rates("Clone 3",rates = c(growth = 0, death=.5))
-sim <- run_down_to_size_by_steps(sim, "Clone 3", 1e3, delta_time = .1)
+
+sim$update_rates("Clone 3",rates = c(growth = 0, death=.3))
+v <- sim$var("Clone 3")
+condition <- v <= 1000
+sim$run_until(condition)
+
+sim$update_rates("Clone 3",rates = c(growth = .1, death=.08))
+sim$run_up_to_size("Clone 3", 1100)
+
 treatment_end <- sim$get_clock()
 
 plot_tissue(sim, num_of_bins = 300)
@@ -67,17 +96,16 @@ ggsave("tissue/tissue_02.pdf", dpi=300, width = 8, height = 8)
 plot_muller(sim) + xlim(20, NA)
 ggsave("tissue/muller_02.pdf", dpi=300, width = 8, height = 8)
 
-# Relapse ####
-
-sim$update_rates("Clone 3",rates = c(growth = .2, death=.001))
-sim <- run_up_to_size_by_steps(sim, "Clone 3", .75e5, delta_time = .1)
+####### RELAPSE #######
+sim$update_rates("Clone 3",rates = c(growth = .5, death=.001))
+sim$run_up_to_size("Clone 3", 60000)
 
 plot_tissue(sim, num_of_bins = 300)
 ggsave("tissue/tissue_03.pdf", dpi=300, width = 8, height = 8)
 plot_muller(sim) + xlim(20, NA)
 ggsave("tissue/muller_03.pdf", dpi=300, width = 8, height = 8)
 
-# Second sampling ####
+####### SAMPLE B #######
 n_w <- n_h <- 15
 ncells <- .99*n_w*n_h
 bbox <- sim$search_sample(c("Clone 3" = ncells), n_w, n_h)
@@ -85,36 +113,10 @@ sim$sample_cells("B", bbox$lower_corner, bbox$upper_corner)
 
 plot_tissue(sim, num_of_bins = 300)
 ggsave("tissue/tissue_04.pdf", dpi=300, width = 8, height = 8)
-plot_muller(sim) + xlim(20, NA)
+plot_muller(sim) + xlim(20, NA) + geom_vline(xintercept = c(treatment_start, treatment_end), color="indianred", linewidth=.3)
 ggsave("tissue/muller_04.pdf", dpi=300, width = 8, height = 8)
 
 sampled_phylogeny <- sim$get_samples_forest()
 sampled_phylogeny$save("data/samples_forest.sff")
 treatment_info <- list(treatment_start=treatment_start, treatment_end=treatment_end)
 saveRDS(treatment_info, "data/treatment_info.rds")
-
-# Recap Plot ####
-muller_plot <- plot_muller(sim)
-piechart <- plot_state(sim)
-timeseries_plot <- plot_timeseries(sim)
-final_sampled_tissue <- plot_tissue(sim)
-plot_phylogeny = plot_forest(sampled_phylogeny) %>%
-  annotate_forest(sampled_phylogeny, 
-                  samples = TRUE, 
-                  MRCAs = TRUE, 
-                  exposures = FALSE, 
-                  facet_signatures = FALSE, 
-                  drivers = FALSE, 
-                  add_driver_label = FALSE)
-layout <- "
-ACBD
-#EE#
-"
-
-summary_patchwork = patchwork::wrap_plots(
-  piechart, muller_plot, final_sampled_tissue, timeseries_plot,plot_phylogeny,
-  guides = 'auto', design = layout
-)
-summary_patchwork
-
-
--- SPN04/03_sequencing.R
+++ SPN04/03_sequencing.R
@@ -12,8 +12,9 @@ phylo_forest <- rRACES::load_phylogenetic_forest("data/phylo_forest.sff")
 
 # Simulate sequencing ####
 chromosomes <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y")
+
 seq_results <- parallel::mclapply(chromosomes, function(c) {
-	simulate_seq(phylo_forest, coverage = 80, chromosomes = c, write_SAM = FALSE)
+	simulate_seq(phylo_forest, coverage = 80, chromosomes = c, write_SAM = TRUE)
 }, mc.cores = parallel::detectCores()) %>% do.call("bind_rows", .)
 #seq_results <- simulate_seq(phylo_forest, coverage = 80)
 
