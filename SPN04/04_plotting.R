707546a088d71847aced7ae28ddac7f771181f2f
Giovanni Santacatterina <65920542+jovoni@users.noreply.github.com>
2024-05-17 10:27:05 +0200

    SPN04 updated plots

---
+++ SPN04/04_plotting.R
@@ -0,0 +1,34 @@
+setwd("~/GitHub/rRACES-examples/SPN04")
+rm(list = ls())
+library(rRACES)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+
+seq_res <- readRDS("data/sequencing.rds")
+
+
+### VAFS ###
+for (sample in c('A', 'B')) {
+  pdf(paste0("tissue/chromosomes/", sample, '_report.pdf'), width = 8, height = 10)
+  for (chr in unique(seq_res$chr)) {
+    p1 <- plot_VAF_gw(seq_res, sample, chromosomes = chr)
+    p2 <- plot_BAF_gw(seq_res, sample, chromosomes = chr)
+    p3 <- plot_DR_gw(seq_res, sample, chromosomes = chr)
+    
+    p <- p1 / p2 / p3
+    p %>% print()  
+  }
+  dev.off()  
+}
+
+### MARGINALS ###
+marginals <- lapply(unique(seq_res$chr), function(c) {
+  print(c)
+  plot_marginals(seq_res, c, colour_by = 'classes')[[1]] + 
+    ggtitle(paste0("Chr", c)) +
+    scale_color_manual(values = c('driver'="black", 'passenger'="#F08080",'pre-neoplastic'= "#5F9EA0"))
+})
+
+p <- wrap_plots(marginals, ncol = 3) + plot_layout(guides = 'collect') & theme(legend.position = 'bottom')
+ggsave("tissue/chromosomes/vaf_marginals.pdf", dpi=300, width = 16, height = 20, plot = p)
