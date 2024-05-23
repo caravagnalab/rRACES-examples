0d2dbd7da03a03353d8f761e831f6527ae936083
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2024-05-23 05:30:20 +0000

    Add plots SPN01

--- SPN01/02_mutations_0.7.1.R
+++ SPN01/02_mutations_0.7.1.R
@@ -7,15 +7,17 @@ library(patchwork)
 # dir.create(path = "data", recursive = TRUE)
 
 # load the samples forest from "samples_forest.sff" and store it in `forest`
-forest <- load_samples_forest("samples_forest_final.sff")
+#forest <- load_samples_forest("/orfeo/cephfs/scratch/cdslab/ggandolfi/races/Use_Cases/SPN01/samples_forest.sff")
+forest <- load_samples_forest("/orfeo/cephfs/scratch/cdslab/ggandolfi/prj_races/rRACES-examples/SPN01/data/samples_forest.sff")
 # building a mutation engine by using the "GRCh38" set-up configuration
+setwd("/orfeo/cephfs/scratch/cdslab/shared/races/")
+
 m_engine <- build_mutation_engine(setup_code = "GRCh38")
 m_engine <- build_mutation_engine(setup_code = "GRCh38", context_sampling = 50)
 
 mu_SNV = 1e-8
 mu_CNA = 1e-11
-## APC gene coordinates 112707518-112846239 
-## Delited region=107707518-117707518
+##112707518-112846239 
 CNA_Clone2 = CNA(type = "D", "5",
                  chr_pos = 107707518, len = 1e7,allele = 0)
 
@@ -47,17 +49,5 @@ m_engine$add_exposure(
 
 print("Mutation engine created")
 phylo_forest <- m_engine$place_mutations(forest, num_of_preneoplatic_mutations = 1000)
-phylo_forest$save("phylo_forest_final.sff")
+phylo_forest$save("/orfeo/cephfs/scratch/cdslab/ggandolfi/prj_races/rRACES-examples/SPN01/data/phylo_forest.sff")
 print("Mutations placed")
-
-chromosomes <- c("5")
-seq_results <-simulate_seq(phylo_forest,chromosomes = c("5"),coverage = 40)
-#seq_results <- parallel::mclapply(chromosomes, function(c) {
-#       simulate_seq(phylo_forest,chromosomes = c,
-#		    coverage = 80) 
-#		    output_dir = "rRACES_SAM_1",update_SAM = TRUE,write_SAM = TRUE)
-#}, mc.cores = parallel::detectCores())
-##%>% do.call("bind_rows", .)
-saveRDS(object =seq_results, file = "seq_results_5.rds")
-seq_results_final<- do.call("bind_rows", seq_results)
-saveRDS(object =seq_results_final, file = "seq_results.rds")
--- SPN01/03_sequencing.R
+++ SPN01/03_sequencing.R
@@ -9,22 +9,25 @@ set.seed(seed)
 
 phylo_forest <- load_phylogenetic_forest("data/phylo_forest.sff")
 
+curr_dir = getwd()
+setwd("/orfeo/cephfs/scratch/cdslab/shared/races")
+
+
+# Simulate sequencing ####
 chromosomes <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y")
 
-# build a basic Illumina sequencer model in which errors occur
-# at rate 4e-3 per base
-basic_seq <- new(BasicIlluminaSequencer, 4e-3)
+# seq_res <- lapply(chromosomes, function(x) {
+# 	simulate_seq(phylo_forest, coverage = 80, chromosomes = x, write_SAM = TRUE, update_SAM = TRUE, rnd_seed = seed)
+# })
+# seq_res <- bind_rows(seq_res)
 
-#seq_results <- parallel::mclapply(chromosomes, function(c) {
-#	simulate_seq(phylo_forest, coverage = 80, chromosomes = c, write_SAM = FALSE)
-#}, mc.cores = parallel::detectCores()) %>% do.call("bind_rows", .)
-
-#saveRDS(object =seq_results ,file = paste0("data/sequencing_homogeneous_growth.rds"))
-# let us simulate a 2.5x sequencing of the four sample
-# on the error-less sequencer
-
-chromosomes <- c("5","20")
 seq_results <- parallel::mclapply(chromosomes, function(c) {
-       simulate_seq(phylo_forest, sequencer = basic_seq,
-		    coverage = 80, chromosomes = c, write_SAM = TRUE)
-}, mc.cores = parallel::detectCores()) %>% do.call("bind_rows", .)
+	simulate_seq(phylo_forest, coverage = 80, chromosomes = c) 
+		     #write_SAM = TRUE, 
+		     #output_dir = "/orfeo/LTS/CDSLab/LT_storage/ggandolfi/races_simulations",rnd_seed = seed)
+}, mc.cores = parallel::detectCores()) 
+seq_results_final<- do.call("bind_rows", seq_results)
+#seq_results <- simulate_seq(phylo_forest, coverage = 80)
+
+saveRDS(seq_results_final, paste0(curr_dir, "/data/seq_results.rds"))
+print("sequencing ended")
