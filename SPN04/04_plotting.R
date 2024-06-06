e73dcefb3f07c37b847ef9614bc6cd5136ac62fd
Giovanni Santacatterina <65920542+jovoni@users.noreply.github.com>
2024-06-06 18:09:36 +0200

    Update SPN04

--- SPN04/04_plotting.R
+++ SPN04/04_plotting.R
@@ -6,26 +6,25 @@ library(ggplot2)
 library(patchwork)
 
 seq_res <- readRDS("data/sequencing.rds")
-
-
 ### VAFS ###
+sample <- 'A'
 for (sample in c('A', 'B')) {
+  print(sample)
   pdf(paste0("tissue/chromosomes/", sample, '_report.pdf'), width = 8, height = 10)
-  for (chr in unique(seq_res$chr)) {
-    p1 <- plot_VAF_gw(seq_res, sample, chromosomes = chr)
-    p2 <- plot_BAF_gw(seq_res, sample, chromosomes = chr)
-    p3 <- plot_DR_gw(seq_res, sample, chromosomes = chr)
-    
-    p <- p1 / p2 / p3
-    p %>% print()  
-  }
-  dev.off()  
+  p1 <- plot_VAF_gw(seq_res, sample)
+  p2 <- plot_BAF_gw(seq_res, sample)
+  p3 <- plot_DR_gw(seq_res, sample)
+
+  p <- p1 / p2 / p3
+  p %>% print()
+  dev.off()
 }
 
 ### MARGINALS ###
+seq_res$chr %>% unique()
 marginals <- lapply(unique(seq_res$chr), function(c) {
   print(c)
-  plot_marginals(seq_res, c, colour_by = 'classes')[[1]] + 
+  plot_marginals(seq_res, c, colour_by = 'classes')[[1]] +
     ggtitle(paste0("Chr", c)) +
     scale_color_manual(values = c('driver'="black", 'passenger'="#F08080",'pre-neoplastic'= "#5F9EA0"))
 })
