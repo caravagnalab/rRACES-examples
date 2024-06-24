9acc2739b5c20cac8b763075b5667f8b6fa8f7c4
valerianilucrezia <valerianilucrezia11@gmail.com>
2024-06-24 15:45:04 +0000

    plot SPN03

--- SPN03/3_plot.R
+++ SPN03/3_plot.R
@@ -2,46 +2,55 @@ library(dplyr)
 library(rRACES)
 library(tidyverse)
 library(patchwork)
+
+#seq_results <- readRDS('/Users/lucreziavaleriani/Desktop/orfeo_LTS/races/SPN03/results/seq_80X.RDS')
 seq_results <- readRDS('/orfeo/LTS/LADE/LT_storage/lvaleriani/races/SPN03/results/seq_80X.RDS')
 samples <- c('Sample.A', 'Sample.B', 'Sample.C', 'Sample.D')
 
+
 gw_plots_baf <- lapply(samples, function(s){
-  rRACES::plot_BAF_gw(seq_results, sample = s, cuts = c(0, 1))
+  rRACES::plot_BAF(seq_results, sample = s, cuts = c(0, 1))
   })
 baf <- patchwork::wrap_plots(gw_plots_baf, nrow = 4)
 
 gw_plots_dr <- lapply(samples, function(s){
-  rRACES::plot_DR_gw(seq_results, sample = s)
+  rRACES::plot_DR(seq_results, sample = s)
 })
 dr <- patchwork::wrap_plots(gw_plots_dr, nrow = 4)
 
 gw_plots_vaf <- lapply(samples, function(s){
-  rRACES::plot_VAF_gw(seq_results, sample = s, cuts = c(0.05, 1))
+  rRACES::plot_VAF(seq_results, sample = s)
 })
 vaf <- patchwork::wrap_plots(gw_plots_vaf, nrow = 4)
 gw_plot <- patchwork::wrap_plots(baf, dr, vaf)
-ggsave(filename = 'plots/gw_plot.png', dpi = 300, plot = gw_plot, width = 14, height = 15, units = 'in')
+ggsave(filename = 'plots/SPN03_seq_plot.png', dpi = 300, plot = gw_plot,  width = 210, height = 297, units = "mm")
+
+seq_results <- seq_results %>% 
+  select(!starts_with("normal")) %>% 
+  filter(causes!="germinal")
 
 
-rRACES::plot_histogram_vaf(seq_results, 
+# Histogram
+hist_class <- rRACES::plot_VAF_histogram(seq_results, 
                    cuts = c(0.05, 1), 
-                   colour_by = 'classes')
-ggsave(filename = 'plots/hist_class.png', dpi = 300, width = 16, height = 8, units = 'in')
+                   labels = seq_results["classes"])
 
-rRACES::plot_histogram_vaf(seq_results, 
+hist_cause <- rRACES::plot_VAF_histogram(seq_results, 
                    cuts = c(0.05, 1), 
-                   colour_by = 'causes')
-ggsave(filename = 'plots/hist_cause.png', dpi = 300, width = 16, height = 8, units = 'in')
+                   labels = seq_results["causes"])
+hist <- hist_class + hist_cause + patchwork::plot_layout(nrow =2)
+ggsave(filename = 'plots/SPN03_histogram.png', plot = hist, dpi = 300,  width = 210, height = 297, units = "mm")
 
 
-patchwork::wrap_plots(rRACES::plot_marginals(seq_results, 
+# Marginals
+marg_class <- patchwork::wrap_plots(rRACES::plot_VAF_marginals(seq_results, 
                                      chromosome = '1', 
-                                     colour_by = 'classes'), guides = 'collect') & theme_bw() + theme(legend.position = 'bottom')
-ggsave(filename = 'plots/marginal_class.png', dpi = 300, width = 10, height = 7, units = 'in')
-
-patchwork::wrap_plots(rRACES::plot_marginals(seq_results, 
+                                     labels = seq_results["classes"]), guides = 'collect') & theme_bw() + theme(legend.position = 'bottom')
+marg_cause <-patchwork::wrap_plots(rRACES::plot_VAF_marginals(seq_results, 
                                      chromosome = '1', 
-                                     colour_by = 'causes'), guides = 'collect') & theme_bw() + theme(legend.position = 'bottom')
-ggsave(filename = 'plots/marginal_cause.png', dpi = 300, width = 10, height = 7, units = 'in')
+                                     labels = seq_results["causes"]), guides = 'collect') & theme_bw() + theme(legend.position = 'bottom')
+
+marg <- patchwork::wrap_plots(marg_class, marg_cause, nrow = 2)
+ggsave(filename = 'plots/SPN03_marginal.png', plot = marg, dpi = 300,  width = 210, height = 297, units = "mm")
 
 
