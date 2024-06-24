2c2e871ef8ecc2ed915f11e350f9d3402c7d7e05
valerianilucrezia <valerianilucrezia11@gmail.com>
2024-06-24 16:53:39 +0200

    modify SPN03

--- SPN03/0_sim_tissue.R
+++ SPN03/0_sim_tissue.R
@@ -3,43 +3,42 @@ library(dplyr)
 library(patchwork)
 library(ggplot2)
 
-sim <- new(Simulation, seed = 12345)
-sim$duplicate_internal_cells <- FALSE
-sim$history_delta <- 1
+set.seed(12345)
+sim <- SpatialSimulation(name = 'SPN03', seed = 12345)
+sim$history_delta <- 1 
 sim$death_activation_level <- 50
 
 # Clone 1
-sim$add_mutant(name = "Clone 1", growth_rates = 0.1, death_rates = 0.01)
+sim$add_mutant(name = "Clone 1", growth_rates = 0.08, death_rates = 0.01)
 sim$place_cell("Clone 1", 500, 500)
 sim$run_up_to_size("Clone 1", 1000)
 
 # Sample A 
-bbox <- sim$search_sample(c("Clone 1" = 200), 20, 20)
+bbox <- sim$search_sample(c("Clone 1" = 300), 25, 25)
 sim$sample_cells("Sample A", bbox$lower_corner, bbox$upper_corner)
 t1 <- plot_tissue(sim)
 
-
 # Clone 2
-sim$add_mutant(name = "Clone 2", growth_rates = 0.2, death_rates = 0.01)
-sim$update_rates(species = "Clone 1", rates = c(growth = 0.08, death = 0.05))
+sim$add_mutant(name = "Clone 2", growth_rates = 0.3, death_rates = 0.01)
+sim$update_rates(species = "Clone 1", rates = c(growth = 0.06, death = 0.01))
 sim$mutate_progeny(sim$choose_cell_in("Clone 1"), "Clone 2")
-sim$run_up_to_size(species = 'Clone 2', 1500)
+sim$run_up_to_size(species = 'Clone 2', 700)
 
-
-bbox <- sim$search_sample(c("Clone 1" = 180, 'Clone 2' = 20), 20, 20)
+bbox <- sim$search_sample(c("Clone 1" = 100, 'Clone 2' = 50), 20, 20)
 sim$sample_cells("Sample B", bbox$lower_corner, bbox$upper_corner)
 t2 <- plot_tissue(sim) 
 
-sim$update_rates(species = "Clone 1", rates = c(growth = 0.05, death = 0.08))
-sim$run_up_to_size(species = 'Clone 2', 5000)
-
+sim$update_rates(species = "Clone 1", rates = c(growth = 0.02, death = 0.03))
+sim$update_rates(species = "Clone 2", rates = c(growth = 0.4, death = 0.005))
+sim$run_up_to_size(species = 'Clone 2', 6000)
 
 # Grow 2
-sim$update_rates(species = "Clone 1", rates = c(growth = 0, death = 0.2))
+sim$update_rates(species = "Clone 1", rates = c(growth = 0.001, death = 0.3))
 sim$run_up_to_size(species = 'Clone 2', 16000)
+muller <- plot_muller(sim)
 
 # Sample C
-bbox <- sim$search_sample(c('Clone 2' = 200), 20, 20)
+bbox <- sim$search_sample(c('Clone 2' = 400), 22, 22)
 sim$sample_cells("Sample C", bbox$lower_corner, bbox$upper_corner)
 t3 <- plot_tissue(sim)
 
@@ -47,34 +46,24 @@ t3 <- plot_tissue(sim)
 sim$add_mutant(name = "Clone 3", growth_rates = 2, death_rates = 0.01)
 sim$update_rates(species = "Clone 2", rates = c(growth = 0.2, death = 0.02))
 sim$mutate_progeny(sim$choose_cell_in("Clone 2"), "Clone 3")
-sim$run_up_to_size("Clone 3", 15000)
+sim$run_up_to_size("Clone 3", 10000)
 muller <- plot_muller(sim)
-ggsave('/orfeo/LTS/LADE/LT_storage/lvaleriani/races/new_FALSE_1_muller_SPN03.png', plot = muller, dpi = 300, units = 'in')
-
-
 
 # Sample D
-bbox <- sim$search_sample(c("Clone 2" = 120, 'Clone 3' = 100), 20, 20)
+bbox <- sim$search_sample(c("Clone 2" = 200, 'Clone 3' = 150), 20, 20)
 sim$sample_cells("Sample D", bbox$lower_corner, bbox$upper_corner)
 t4 <- plot_tissue(sim)
 
 # Forest
 forest <- sim$get_samples_forest()
-#forest$save("/orfeo/LTS/LADE/LT_storage/lvaleriani/races/SPN03/results/samples_forest.sff")
+forest$save("/orfeo/LTS/LADE/LT_storage/lvaleriani/races/SPN03/results/samples_forest.sff")
 
 plt_forest <- plot_forest(forest) %>%
   annotate_forest(forest)
-ggsave('/orfeo/LTS/LADE/LT_storage/lvaleriani/races/new_FALSE_1_SPN03_forest.png', plt_forest)
-
 
 piechart <- plot_state(sim)
 timeseries <- plot_timeseries(sim)
 
 # Final plot
-pl <- t1 + t2 + t3 + t4  + piechart + timeseries + muller + plt_forest + plot_layout(design = 'ABCD
-                                                                                         EFGG
-                                                                                         HHHH
-                                                                                         HHHH
-                                                                                         HHHH
-                                                                                         HHHH')
-ggsave('/orfeo/LTS/LADE/LT_storage/lvaleriani/races/new_FALSE_1_SPN03.png', plot = pl, height = 12, width = 10, dpi = 300, units = 'in')
+pl <- t1 + t2 + t3 + t4  + piechart + timeseries + muller + plt_forest + plot_layout(design = 'ABCD\nEFGG\nHHHH\nHHHH\nHHHH') 
+ggsave('/orfeo/LTS/LADE/LT_storage/lvaleriani/races/SPN03/plots/SPN03_tissue.png', plot = pl, width = 210, height = 297, units = "mm", dpi = 300)
