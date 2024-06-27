911ea426db2ea405b918534b6930832715fa3feb
valerianilucrezia <valerianilucrezia11@gmail.com>
2024-06-27 10:03:23 +0000

    modify plot SPN03

--- SPN03/3_plot.R
+++ SPN03/3_plot.R
@@ -7,50 +7,25 @@ library(patchwork)
 seq_results <- readRDS('/orfeo/LTS/LADE/LT_storage/lvaleriani/races/SPN03/results/seq_80X.RDS')
 samples <- c('Sample.A', 'Sample.B', 'Sample.C', 'Sample.D')
 
+g_seq <- seq_results %>% filter(classes=="germinal")
+for (sample in samples) {
+  print(sample)
+  
+  p1 <- plot_VAF(g_seq, sample)
+  p2 <- plot_BAF(g_seq, sample)
+  p3 <- plot_DR(g_seq, sample)
 
-gw_plots_baf <- lapply(samples, function(s){
-  rRACES::plot_BAF(seq_results, sample = s, cuts = c(0, 1))
-  })
-baf <- patchwork::wrap_plots(gw_plots_baf, nrow = 4)
+  p <- p1 / p2 / p3
+  ggsave(paste0("plots/SPN03_",sample, '_report.png'),plot = p, dpi=300, width = 8, height = 10)
+}
 
-gw_plots_dr <- lapply(samples, function(s){
-  rRACES::plot_DR(seq_results, sample = s)
-})
-dr <- patchwork::wrap_plots(gw_plots_dr, nrow = 4)
-
-gw_plots_vaf <- lapply(samples, function(s){
-  rRACES::plot_VAF(seq_results, sample = s)
-})
-vaf <- patchwork::wrap_plots(gw_plots_vaf, nrow = 4)
-gw_plot <- patchwork::wrap_plots(baf, dr, vaf)
-ggsave(filename = 'plots/SPN03_seq_plot.png', dpi = 300, plot = gw_plot,  width = 210, height = 297, units = "mm")
-
-seq_results <- seq_results %>% 
-  select(!starts_with("normal")) %>% 
-  filter(causes!="germinal")
-
-
-# Histogram
-hist_class <- rRACES::plot_VAF_histogram(seq_results, 
-                   cuts = c(0.05, 1), 
-                   labels = seq_results["classes"])
-
-hist_cause <- rRACES::plot_VAF_histogram(seq_results, 
-                   cuts = c(0.05, 1), 
-                   labels = seq_results["causes"])
-hist <- hist_class + hist_cause + patchwork::plot_layout(nrow =2)
-ggsave(filename = 'plots/SPN03_histogram.png', plot = hist, dpi = 300,  width = 210, height = 297, units = "mm")
-
-
-# Marginals
-marg_class <- patchwork::wrap_plots(rRACES::plot_VAF_marginals(seq_results, 
-                                     chromosome = '1', 
-                                     labels = seq_results["classes"]), guides = 'collect') & theme_bw() + theme(legend.position = 'bottom')
-marg_cause <-patchwork::wrap_plots(rRACES::plot_VAF_marginals(seq_results, 
-                                     chromosome = '1', 
-                                     labels = seq_results["causes"]), guides = 'collect') & theme_bw() + theme(legend.position = 'bottom')
-
-marg <- patchwork::wrap_plots(marg_class, marg_cause, nrow = 2)
-ggsave(filename = 'plots/SPN03_marginal.png', plot = marg, dpi = 300,  width = 210, height = 297, units = "mm")
-
-
+pdf("plots/SPN03_vaf_report.pdf", width = 8, height = 10)
+s_seq <- seq_results %>% filter(classes!="germinal")
+for (c in unique(s_seq$chr)) {
+    p_marg <- plot_VAF_marginals(s_seq, chromosomes = c, samples = samples, labels = s_seq["classes"]) 
+    p_hist <- plot_VAF_histogram(s_seq, chromosomes = c, samples = samples, labels = s_seq["classes"], cuts = c(0.02, 1)) + xlim(0,1)
+    p <- (wrap_plots(p_marg, ncol = 3) + p_hist) + 
+      plot_layout(guides = 'collect', design = 'ABC\nDEF\nGGG\nGGG') + plot_annotation(title = (paste("Chromosome", c))) & theme(legend.position = 'bottom')
+    print(p)
+}
+dev.off()
