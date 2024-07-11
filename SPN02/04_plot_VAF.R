76a55f52d3bb9d8a9fef44291fa12d9091e95574
Virginia Gazziero <virginia.gazziero@areasciencepark.it>
2024-07-11 09:44:45 +0000

    baf and logDR SPN02

--- SPN02/04_plot_VAF.R
+++ SPN02/04_plot_VAF.R
@@ -14,11 +14,11 @@ samples = c("A", "B")
 chromosomes <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y")
 
 seq_res_filt = seq_res %>% filter(classes != "germinal")
-
+seq_res_germ = seq_res %>% filter(classes == "germinal")
 lapply(samples, function(s) {
     vaf = plot_VAF(seq_res_filt, s)
-    baf = plot_BAF(seq_res_filt, s)
-    dr = plot_DR(seq_res_filt, s)
+    baf = plot_BAF(seq_res_germ, s)
+    dr = plot_DR(seq_res_germ, s)
 
     p = vaf / baf / dr
 
@@ -32,7 +32,7 @@ for (c in unique(seq_res_filt$chr)) {
     print(c)
     p_marg <- plot_VAF_marginals(seq_res_filt, chromosomes = c, samples = samples, labels = seq_res_filt["classes"])
     p_hist <- plot_VAF_histogram(seq_res_filt, chromosomes = c, samples = samples, labels = seq_res_filt["classes"], cuts = c(0.02, 1))
-    p <- (wrap_plots(p_marg, ncol=3)+p_hist)+ plot_layout(guides = 'collect') & theme(legend.position = 'bottom') & ggtitle(paste("Chromosome", c))
+    p <- (wrap_plots(p_marg, ncol=2)+p_hist)+ plot_layout(guides = 'collect') & theme(legend.position = 'bottom') & ggtitle(paste("Chromosome", c))
     #p_marg <- lapply(p_marg, function(p) p + ggtitle(paste("Chromosome", c)))
     #p <- wrap_plots(list(p_marg,p_hist),ncol = 3, nrow=2) + plot_layout(guides = 'collect') & theme(legend.position = 'bottom')
     print(p)
