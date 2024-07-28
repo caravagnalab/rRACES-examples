3dc671399be6ffb3bc2227e91530f4a099341aa0
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2024-07-28 08:53:45 +0000

    Update to new version

--- SPN01/03_sequencing_tumor.R
+++ SPN01/03_sequencing_tumor.R
@@ -18,11 +18,13 @@ chromosomes <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","1
 #no_error_seq <- ErrorlessIlluminaSequencer()
 basic_seq <- BasicIlluminaSequencer(1e-3) ## only for testing purpose
 
+
+
 seq_results <- parallel::mclapply(chromosomes, function(c) {
-	simulate_seq(phylo_forest, chromosomes = c, coverage = 100,write_SAM = TRUE,
-		     sequencer = basic_seq, insert_size_mean = 350, insert_size_stddev = 10, , output_dir = "/orfeo/LTS/CDSLab/LT_storage/ggandolfi/races_simulations/FINAL_DATA/sequencing_100X_basic_error_paired_350_3tumor",
+	simulate_seq(phylo_forest, chromosomes = c, coverage = 100,write_SAM = TRUE,read_size =150, 
+		     sequencer = basic_seq, insert_size_mean = 350, insert_size_stddev = 10, , output_dir = "/orfeo/LTS/CDSLab/LT_storage/ggandolfi/races_simulations/FINAL_DATA/sequencing_100X_basic_error_paired_350_3tumor_new1",
 	update_SAM =TRUE, with_normal_sample =FALSE)
-}, mc.cores = parallel::detectCores())
+}, mc.cores = 8)
 
 seq_results_final<- do.call("bind_rows", seq_results)
 saveRDS(seq_results_final, "/orfeo/cephfs/scratch/cdslab/ggandolfi/prj_races/rRACES-examples/SPN01/data/seq_results_100X_basic_error_350_3tumor.rds")
