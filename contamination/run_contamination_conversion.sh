3d647d99a5082f4b059a3b8d4341aea3f4c5c3d3
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2024-08-08 10:14:57 +0000

    Finalized pipeline for dilution

--- contamination/run_contamination_conversion.sh
+++ contamination/run_contamination_conversion.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=72:00:00
 #SBATCH --output=contamination_%A_%a.out
 #SBATCH --error=contamination_%A_%a.err
-#SBATCH --array=1-24
+#SBATCH --array=12-13
 
 module load samtools
 
@@ -17,8 +17,8 @@ module load samtools
 # define the folders 
 # sequencing folder
 
-races_dir="/orfeo/cephfs/scratch/area/vgazziero/CDSlab/rRaces/test/contamination_downsampling_final/simulation/sequencing_200X_basic_error_paired_350_1tumor_new_1"
-races_dir_n="/orfeo/cephfs/scratch/area/vgazziero/CDSlab/rRaces/test/contamination_downsampling_final/simulation/sequencing_160X_basic_error_paired_350_1normal_new_1_germline"
+races_dir="$1" #"/orfeo/cephfs/scratch/area/vgazziero/CDSlab/rRaces/test/contamination_downsampling_final/simulation/sequencing_200X_basic_error_paired_350_1tumor_new_1"
+races_dir_n="$2" #"/orfeo/cephfs/scratch/area/vgazziero/CDSlab/rRaces/test/contamination_downsampling_final/simulation/sequencing_160X_basic_error_paired_350_1normal_new_1_germline"
 
 
 # races_dir="/orfeo/LTS/CDSLab/LT_storage/ggandolfi/races_simulations/CHECK_PURITY/sequencing_100X_basic_error_paired_350_1tumor_new_1" #"path/to/dir/where/sam/from/races/tumor/are/sequenced"
@@ -26,7 +26,7 @@ races_dir_n="/orfeo/cephfs/scratch/area/vgazziero/CDSlab/rRaces/test/contaminati
 downsampling=${races_dir}/downsampling
 # tmp folder
 races_tmp=${downsampling}/tmp/
-tmp_n=${races_tmp}normal
+tmp_n=${races_tmp}/normal
 # normal folder 
 # races_dir_n="/orfeo/LTS/CDSLab/LT_storage/ggandolfi/races_simulations/CHECK_PURITY/sequencing_80X_basic_error_paired_350_1normal_new_1" #"path/to/dir/where/sam/from/races/normal/are/sequenced/"
 
@@ -50,14 +50,24 @@ samples_tmp_folder_new_id=$(echo $samples | sed "s@[^ ]*@${races_tmp}&@g" | sed 
 mkdir -p ${samples_tmp_folder_new_id}
 
 # split sam by chromosome per sample
-samtools split -f ${races_tmp}/%\!/splitted_sorted_bam/%*_%\!.bam --threads 8 ${races_dir}/${chr}.sam 
-echo "splitted sam"
+# Check if a representative split BAM file already exists
+#chr_21_SPN01_Sample_1.bam
+if [ -f ${races_tmp}/splitted_sorted_bam/${chr}*.bam ]; then
+    echo "Split BAM files for ${chr} already exist, skipping samtools split."
+else
+    # Run samtools split if the representative split BAM file does not exist
+    samtools split -f ${races_tmp}/%\!/splitted_sorted_bam/%*_%\!.bam --threads 8 ${races_dir}/${chr}.sam
+    echo "BAM files for ${chr} split"
+fi
+
+#samtools split -f ${races_tmp}/%\!/splitted_sorted_bam/%*_%\!.bam --threads 8 ${races_dir}/${chr}.sam 
+#echo "splitted sam"
 # define the purity and final coverage you want to get
-purity=0.70
+purity="$3"
 frac=($(echo $purity | cut -f 2 -d "."))
-original_depth_tumor=200
-original_depth_normal=160
-new_depth_tumor=100
+original_depth_tumor="$4"
+original_depth_normal="$5"
+new_depth_tumor="$6"
 
 new_bam=${downsampling}/purity_${frac}_coverage_${new_depth_tumor}
 new_bam_samples=$(echo $samples | sed "s@[^ ]*@${new_bam}/&@g")
@@ -68,25 +78,50 @@ mkdir -p $new_bam_samples
 # sort normal sample 
 mkdir  -p ${races_tmp}/samtools_sort_tmp
 mkdir -p ${tmp_n}/splitted_sorted_bam
-samtools sort $races_dir_n/${chr}.sam -T ${races_tmp}/samtools_sort_tmp -o ${tmp_n}/splitted_sorted_bam/${chr}.sorted.bam
-echo "bam sorted"
+# Check if the sorted BAM file already exists
+if [ -f ${tmp_n}/splitted_sorted_bam/${chr}.sorted.bam ]; then
+    echo "Sorted BAM file for ${chr} already exists, skipping samtools sort."
+else
+    # Run samtools sort if the sorted BAM file does not exist
+    samtools sort $races_dir_n/${chr}.sam -T ${races_tmp}/samtools_sort_tmp -o ${tmp_n}/splitted_sorted_bam/${chr}.sorted.bam
+    echo "bam sorted for ${chr}"
+fi
+
+#samtools sort $races_dir_n/${chr}.sam -T ${races_tmp}/samtools_sort_tmp -o ${tmp_n}/splitted_sorted_bam/${chr}.sorted.bam
+#echo "bam sorted"
 #
-new_id="SPN01_downsampled_07P" #"choose_new_id"
+new_id="$7" #"SPN01_downsampled_07P" "choose_new_id"
 
 # modify the ID before merging
 mkdir -p ${tmp_n}/new_id
-samtools addreplacerg \
-    -r ID:${new_id} \
-    -r SM:${new_id} \
-    -r PL:ILLUMINA \
-    -o ${tmp_n}/new_id/${chr}.sorted.ID.bam \
-    ${tmp_n}/splitted_sorted_bam/${chr}.sorted.bam ## required to have the same ID once merged
-echo "id normal changed"
+
+# Check if the BAM file with the new ID already exists
+if [ -f ${tmp_n}/new_id/${chr}.sorted.ID.bam ]; then
+    echo "BAM file with new ID for ${chr} already exists, skipping samtools addreplacerg."
+else
+    # Run samtools addreplacerg if the BAM file with the new ID does not exist
+    samtools addreplacerg \
+        -r ID:${new_id} \
+        -r SM:${new_id} \
+        -r PL:ILLUMINA \
+        -o ${tmp_n}/new_id/${chr}.sorted.ID.bam \
+        ${tmp_n}/splitted_sorted_bam/${chr}.sorted.bam
+    echo "id normal changed for ${chr}"
+fi
+
+
+#samtools addreplacerg \
+#    -r ID:${new_id} \
+#    -r SM:${new_id} \
+#    -r PL:ILLUMINA \
+#    -o ${tmp_n}/new_id/${chr}.sorted.ID.bam \
+#    ${tmp_n}/splitted_sorted_bam/${chr}.sorted.bam ## required to have the same ID once merged
+#echo "id normal changed"
 
 # launch processing of the tumour bam on each sample
 sample_list=${races_tmp}${chr}/sample_names.txt
 n_sample_job=$(wc -l < ${sample_list})
-sbatch --array=1-$(($n_sample_job)) /orfeo/cephfs/scratch/area/vgazziero/CDSlab/rRaces/rRACES-examples/contamination/tumour_processing.sh ${sample_list} ${chr} ${races_tmp} ${original_depth_tumor} ${original_depth_normal} ${purity} ${new_depth_tumor} ${frac}
+sbatch --array=1-$(($n_sample_job)) tumour_processing.sh ${sample_list} ${chr} ${races_tmp} ${original_depth_tumor} ${original_depth_normal} ${purity} ${new_depth_tumor} ${new_id} ${new_bam}
 
 ## sort tumor sample
 
--- contamination/tumour_processing.sh
+++ contamination/tumour_processing.sh
@@ -11,15 +11,17 @@
 
 module load samtools 
 
-sample_list=${1}
-chr=${2}
-races_tmp=${3}
-original_depth_tumor=${4}
-original_depth_normal=${5}
-purity=${6}
-new_depth_tumor=${7}
-frac=${8}
-
+sample_list="$1"
+chr="$2"
+races_tmp="$3"
+original_depth_tumor="$4"
+original_depth_normal="$5"
+purity="$6"
+new_depth_tumor="$7"
+frac=($(echo $purity | cut -f 2 -d "."))
+new_id="$8"
+new_bam="$9"
+tmp_n=${races_tmp}/normal
 sample=$(sed -n "$((SLURM_ARRAY_TASK_ID))p" $sample_list)
 echo "Eseguendo il job: $job"
 
@@ -29,17 +31,30 @@ new_id_out=${races_tmp}/${sample}/new_id/${chr}_${sample}.sorted.ID.bam
 sam_splitted=${races_tmp}/${sample}/splitted_sorted_bam/${chr}_${sample}.bam
 
 # sorting
-samtools sort -o ${sorted_file} -T ${races_tmp}/samtools_sort_tmp ${sam_splitted}
-echo "tumor bam sorted"
+if [ -f ${sorted_file} ]; then
+    echo "Sorted BAM files for ${chr} and ${sample} already exist, skipping samtools sort."
+else
+    # Run samtools sort if the representative sorted BAM file does not exist
+    samtools sort -o ${sorted_file} -T ${races_tmp}/samtools_sort_tmp ${sam_splitted}
+    echo "BAM files for ${chr} and ${sample} sorted"
+fi
+
 # ID replacing
-samtools addreplacerg \
+if [ -f ${new_id_out} ]; then
+	echo "BAM file with new ID for ${chr} and ${sample} already exists, skipping samtools addreplacerg."
+else
+   # Run samtools addreplacerg if the BAM file with the new ID does not exist
+   samtools addreplacerg \
     -r ID:${new_id} \
     -r SM:${new_id} \
     -r PL:ILLUMINA \
     -o ${new_id_out} \
     ${sorted_file} ## required to have the same ID once merged
-echo "tumor bam id changed"
-/orfeo/cephfs/scratch/area/vgazziero/CDSlab/rRaces/rRACES-examples/contamination/downsampling.sh \
+  echo "id changed for ${chr} and ${sample}" 
+fi
+
+# Run downsampling
+bash downsampling.sh \
     -t ${new_id_out} \
     -n ${tmp_n}/new_id/${chr}.sorted.ID.bam \
     -i $original_depth_tumor \
@@ -48,6 +63,7 @@ echo "tumor bam id changed"
     -d $new_depth_tumor \
     -o ${new_bam}/${sample}/${chr}_${sample}.purity_${frac}_coverage_${new_depth_tumor}.bam
 mkdir -p ${races_tmp}/${sample}/bam2fastq
-samtools fastq -1 ${races_tmp}/${sample}/bam2fastq/${chr}_${sample}.R1.fastq \
-    -2 ${races_tmp}/${sample}/bam2fastq/${chr}_${sample}.R2.fastq \
-    -0 ${races_tmp}/${sample}/bam2fastq/${chr}_${sample}.unpaired.fastq -s ${races_tmp}/${sample}/bam2fastq/${chr}_${sample}.singleton.fastq -N ${new_bam}/${sample}/${chr}_${sample}.purity_${frac}_coverage_${new_depth_tumor}.bam
+samtools sort -n ${new_bam}/${sample}/${chr}_${sample}.purity_${frac}_coverage_${new_depth_tumor}.bam -T ${races_tmp}/samtools_sort_tmp | \
+	samtools fastq -1 ${races_tmp}/${sample}/bam2fastq/${chr}_${sample}.R1.fastq \
+          -2 ${races_tmp}/${sample}/bam2fastq/${chr}_${sample}.R2.fastq \
+          -0 ${races_tmp}/${sample}/bam2fastq/${chr}_${sample}.unpaired.fastq -s ${races_tmp}/${sample}/bam2fastq/${chr}_${sample}.singleton.fastq -N
