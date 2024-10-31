9b3c78069af3f292ed9b69dba2c600aedca64cfd
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2024-10-31 19:17:35 +0100

    update scripts for report (old version)

--- SPN01/3_plotting.R
+++ SPN01/3_plotting.R
@@ -4,18 +4,20 @@ library(dplyr)
 library(patchwork)
 library(ggplot2)
 library(CNAqc)
+library(ggrepel)
 #source("/orfeo/cephfs/scratch/cdslab/ggandolfi/prj_races/rRACES-examples/SPN01/scripts/my_functions/plot_genome_wide.R")
 source("/orfeo/cephfs/scratch/cdslab/ggandolfi/prj_races/rRACES-examples/plotting/spn_blueprint/utils.R")
 source("/orfeo/cephfs/scratch/cdslab/ggandolfi/prj_races/rRACES-examples/plotting/spn_blueprint/plot_genome_wide.R")
-seq_results <- readRDS("new_simulation/sim5/monday_meeting/WGD_post/seq_res_new.rds")
-phylo_forest <- load_phylogenetic_forest("new_simulation/sim5/monday_meeting/WGD_post/phylo_forest.sff")
+#source("/orfeo/cephfs/scratch/cdslab/ggandolfi/prj_races/rRACES-examples/plotting/spn_blueprint/signature_palette.R")
+seq_results <- readRDS("seq_res_smaller_200X.rds")
+phylo_forest <- load_phylogenetic_forest("phylo_forest_smaller.sff")
 
 samples <- phylo_forest$get_samples_info()[["name"]] %>% sort()
 print(samples)
-pdf("genome_wide.pdf", width = 10, height = 12)
+pdf("genome_wide_200X.pdf", width = 10, height = 12)
 plots_gw <- list()
 
-cov = 80
+cov = 200
 error_rate = 1e-3
 tumour_type = "COAD"
 germline_sub = "default"
@@ -25,24 +27,14 @@ germline_sub = "default"
 ## Create an empty dataframe
 #df <- data.frame(matrix(ncol = length(colnames), nrow = length(rownames)))
 
-#plots_insp <- list()
 for (i in samples){
 	x <- races2cnaqc(seq_results=seq_results,
 			 phylo_forest=phylo_forest,
 			 sample_id=i,
 			 ref="GRCh38",
-			 purity=1)
-	print(x$cna)
-	saveRDS(x,file=paste0("CNAqc_",i,".rds"))
-        #g_seq <- seq_results %>% filter(classes=="germinal")
-        #seg <- plot_segments(x = x,highlight = c("1:0","1:1","2:1","2:2"))
-        #dr <- plot_DR_n(seq_res = g_seq,sample = i)
-        #baf <- plot_BAF_n(seq_res = g_seq,sample = i)
-        #vaf_histo <- plot_data_histogram(x, which = 'VAF')
-        #dp_histo <- plot_data_histogram(x, which = 'DP')
-
-        #p1 <- vaf_histo/dp_histo+plot_layout(design="AAAAA\nAAAAA\nAAAAA\nBBBBB\nBBBBB\nBBBBB")
-        #p2 <- dr/baf/seg+plot_layout(design="AAAAA\nAAAAA\nBBBBB\nBBBBB\nCCCCC\nCCCCC")
+			 purity=0.9)
+	#saveRDS(x,file=paste0("CNAqc_",i,".rds"))
+	#x <- readRDS(paste0("CNAqc_",i,".rds"))
 
 
 	gw <- genome_wide_plots(x,seq_results,i)
@@ -53,36 +45,99 @@ for (i in samples){
 							     "\nSequencing Errore rate: ", error_rate,
 							     "\nTumour type: ", tumour_type,
 							     "\nGermline subject :", germline_sub))
-	#plots_insp[[i]] <-inspect_segment(x)+ggtitle(label=i)
+
 	print(plots_gw[[i]])
 	seg <- wrap_plots(gw[[3]])
 	print(seg)
+
+}
+#
+#
+#
+#
+#
+##saveRDS(plots_gw,"page3_gw_1.rds")
+##plot_vafs <- wrap_plots(plots_insp,ncol =1,nrow=length(samples))
+#
+##saveRDS(plot_vafs,"page3_vafs.rds")
+#dev.off()
+
+
+
+expousures_table <- phylo_forest$get_exposures() %>% dplyr::rename(causes=signature)
+#pdf("chromosome_vafs.pdf",width = 12, height = 6)
+#chroms <- c("22","5","17","3")
+s_seq <- seq_results %>% filter(classes!="germinal")
+s_seq_long <- s_seq %>% rRACES::seq_to_long()
+for (c in unique(seq_results$chr)) {
+  colors <- get_classes_colors(unique(s_seq$classes))
+  plot_vaf <- s_seq_long %>%
+    filter(chr==c) %>%
+    filter(VAF>=0.02) %>%
+    ggplot(aes(x=VAF,fill=classes)) +geom_histogram(binwidth = 0.01) +
+    xlim(c(0,1))+
+    facet_wrap(~ sample_name, ncol=1,scales = "free")+
+    ggplot2::ggtitle(label = paste0("Chromosome ",c)) +
+    CNAqc:::my_ggplot_theme()+
+    ggplot2::scale_fill_manual(values =colors)
+    
+  p_marg <- plot_VAF_marginals(s_seq, chromosomes = c, samples = samples, labels = s_seq["classes"])
+  if (length(p_marg)<10){
+    n <- length(p_marg)+1
+    for (i in c(n:10)){
+      p_marg[[i]]<-ggplot2::ggplot()
+    }
+  }
+  layout <- 'AAABB\nAAABB\nAAABB\nAAABB\nAAABB'
+  p <- wrap_plots(p_marg,design='AAFF\nAAFF\nBBGG\nBBGG\nCCHH\nCCHH\nDDII\nDDII\nEEJJ\nEEJJ')/plot_vaf+
+          plot_layout(guides = 'collect', design = layout) + plot_annotation(title = (paste("Chromosome", c))) & theme(legend.position = 'bottom')
+  #p_marg <- lapply(p_marg, function(p) p + ggtitle(paste("Chromosome", c)))
+  #p <- wrap_plots(list(p_marg,p_hist),ncol = 3, nrow=2) + plot_layout(guides = 'collect') & theme(legend.position = 'bottom')
+  #print(plot_vaf)
+  print(p)
 }
 
 
 
+##### Plot VAF per exposures ########
 
-
-#saveRDS(plots_gw,"page3_gw_1.rds")
-#plot_vafs <- wrap_plots(plots_insp,ncol =1,nrow=length(samples))
-
-#saveRDS(plot_vafs,"page3_vafs.rds")
+exposure_prop <- phylo_forest$get_exposures() %>% 
+  ggplot(aes(fill=signature, y=exposure,x=type)) + 
+  geom_bar(position="fill", stat="identity")+
+  coord_flip()+
+  facet_wrap(~time)+
+  CNAqc:::my_ggplot_theme()
+plot_vaf <- s_seq_long %>%
+  filter(VAF>=0.02) %>%
+  filter(grepl("SBS|ID", causes)) %>%
+  filter(!grepl("error",causes)) %>%
+  inner_join(expousures_table,by="causes") %>%
+  ggplot(aes(x=VAF, fill= causes,alpha=exposure)) +geom_histogram(binwidth = 0.01) +
+  xlim(c(0,1))+
+  facet_grid(sample_name ~ causes, scales = "free_y")+
+  ggplot2::scale_alpha_continuous(range = c(0.1, 1)) + 
+  ggplot2::theme_bw() +
+  CNAqc:::my_ggplot_theme()
+exposure_prop +plot_vaf + plot_layout(design="A\nA\n#\nB\nB\nB")+
+	plot_annotation(title = "Mutational signatures")
 dev.off()
 
 
-chroms <- c("22","5","17","3")
+
+
 #chroms <- unique(seq_results$chr)
 #chroms <- c("1","2","3","4","5","6","7","8", "9","10","11","12","13","14","15",
 #	    "16","17","18","19","20","21","22","X","Y")
-pdf("chromosome_vafs_1.pdf",width = 8, height = 10)
-vaf_plots <- list()
-for (c in chroms){
-        vaf_plots[[c]] <- squareplot(seq_res=seq_results,
-			       samples_list=samples,
-			       chrom=c)
-	print(c)
-	print(vaf_plots[[c]])
-	#print(c)
-}
-dev.off()
+#pdf("chromosome_vafs_1.pdf",width = 8, height = 10)
+#vaf_plots <- list()
+#for (c in chroms){
+#        vaf_plots[[c]] <- squareplot(seq_res=seq_results,
+#			       samples_list=samples,
+#			       chrom=c)
+#	print(c)
+#	print(vaf_plots[[c]])
+#	#print(c)
+#}
+#dev.off()
 #saveRDS(vaf_plots,"page4_vafs.rds")
+
