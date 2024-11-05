ffdc3060de1f4d62009d2edc297a6bd0969b2b6e
azadsadr <azad.sadr@proton.me>
2024-11-05 11:24:44 +0100

    modified plotting script

--- SPN06/plot.R
+++ SPN06/plot.R
@@ -31,17 +31,14 @@ print(samples)
 
 pdf("output/genome_wide_80X.pdf", width = 10, height = 12)
 
-
-
-
-
 plots_gw <- list()
 
-cov = 800
+cov = 80
 error_rate = 1e-3
 tumour_type = "LUAD"
 germline_sub = "default"
 
+#-------------------------------------------------------------------------------
 for (i in samples) {
   
   x <- races2cnaqc(
@@ -51,7 +48,6 @@ for (i in samples) {
     ref="GRCh38",
     purity=0.9
   )
-  
   
   gw <- genome_wide_plots(x, seq_results, i)
   plots_gw[[i]] <- wrap_plots(gw[[1]], gw[[2]]) + 
@@ -65,13 +61,69 @@ for (i in samples) {
   print(plots_gw[[i]])
   seg <- wrap_plots(gw[[3]])
   print(seg)
+}
+
+#-------------------------------------------------------------------------------
+expousures_table <- phylo_forest$get_exposures() %>% dplyr::rename(causes=signature)
+s_seq <- seq_results %>% filter(classes!="germinal")
+s_seq_long <- s_seq %>% rRACES::seq_to_long()
+
+for (c in unique(seq_results$chr)) {
   
+  colors <- get_classes_colors(unique(s_seq$classes))
+  
+  plot_vaf <- s_seq_long %>%
+    filter(chr==c) %>%
+    filter(VAF>=0.02) %>%
+    ggplot(aes(x=VAF,fill=classes)) +geom_histogram(binwidth = 0.01) +
+    xlim(c(0,1))+
+    facet_wrap(~ sample_name, ncol=1,scales = "free")+
+    ggplot2::ggtitle(label = paste0("Chromosome ",c)) +
+    CNAqc:::my_ggplot_theme()+
+    ggplot2::scale_fill_manual(values =colors)
+  
+  p_marg <- plot_VAF_marginals(s_seq, chromosomes = c, samples = samples, labels = s_seq["classes"])
+  
+  if (length(p_marg)<10) {
+    n <- length(p_marg)+1
+    for (i in c(n:10)){
+      p_marg[[i]]<-ggplot2::ggplot()
+    }
+  }
+  
+  layout <- 'AAABB\nAAABB\nAAABB\nAAABB\nAAABB'
+  p <- wrap_plots(p_marg, design='AAFF\nAAFF\nBBGG\nBBGG\nCCHH\nCCHH\nDDII\nDDII\nEEJJ\nEEJJ') / plot_vaf + 
+    plot_layout(guides = 'collect', design = layout) + plot_annotation(title = (paste("Chromosome", c))) & theme(legend.position = 'bottom')
+
+  print(p)
 }
 
 
+#-------------------------------------------------------------------------------
+exposure_prop <- phylo_forest$get_exposures() %>% 
+  ggplot( aes(fill=signature, y=exposure,x=type) ) + 
+  geom_bar(position="fill", stat="identity")+
+  coord_flip()+
+  facet_wrap(~time)+
+  CNAqc:::my_ggplot_theme()
+
+plot_vaf <- s_seq_long %>%
+  filter(VAF>=0.02) %>%
+  filter(grepl("SBS|ID", causes)) %>%
+  filter(!grepl("error",causes)) %>%
+  inner_join(expousures_table,by="causes") %>%
+  ggplot(aes(x=VAF, fill= causes,alpha=exposure)) +geom_histogram(binwidth = 0.01) +
+  xlim(c(0,1))+
+  facet_grid(sample_name ~ causes, scales = "free_y")+
+  ggplot2::scale_alpha_continuous(range = c(0.1, 1)) + 
+  ggplot2::theme_bw() +
+  CNAqc:::my_ggplot_theme()
+
+exposure_prop + plot_vaf + plot_layout(design="A\nA\n#\nB\nB\nB") + 
+  plot_annotation(title = "Mutational signatures")
+
 
 dev.off()
-
 
 
 #-------------------------------------------------------------------------------
