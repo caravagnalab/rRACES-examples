aaf0cfa9b8fdd8d20eb9c9af48ba37179dab7e3c
valerianilucrezia <valerianilucrezia11@gmail.com>
2025-03-20 12:48:29 +0100

    minor to main plotting

--- report/plotting/signature_rRACES.R
+++ report/plotting/signature_rRACES.R
@@ -7,8 +7,20 @@ library(ggplot2)
 library(patchwork)
 source("/orfeo/cephfs/scratch/cdslab/ggandolfi/prj_races/REPO_UPDATED/rRACES-examples/report/plotting/utils.R")
 
-
-
+get_colors_for <- function(values, pal_name = "Set3") {
+  colors <- NULL
+  if (length(values) > 0) {
+    num_of_values <- values %>% length()
+    if (num_of_values < 3) {
+      colors <- RColorBrewer::brewer.pal(3, pal_name)
+      colors <- colors[seq_len(num_of_values)]
+    } else {
+      colors <- RColorBrewer::brewer.pal(num_of_values, pal_name)
+    }
+    names(colors) <- values
+  }
+  return(colors)
+}
 
 plot_exposure_evolution <- function(sample_forest,phylo_forest,snapshot){
   simulation <- rRACES::recover_simulation(snapshot)
@@ -16,6 +28,8 @@ plot_exposure_evolution <- function(sample_forest,phylo_forest,snapshot){
   muller <- plot_muller(simulation,color_map = color_map_clones)
   
   signature <- phylo_forest$get_exposures()
+  palette_signature <-  get_colors_for(signature %>% pull(signature) %>% unique())
+  
   df_sign <- get_exposure_ends(phylo_forest)
   start_time <- 0
   end_time <- max(df_sign$end_time)
@@ -33,8 +47,9 @@ plot_exposure_evolution <- function(sample_forest,phylo_forest,snapshot){
   #
   #
   sign_1 <- ggplot(df_final, aes(x = factor(time), stratum = signature, alluvium = signature, y = exposure)) +
-    geom_alluvium(aes(fill = signature), width = 0.001, curve_type = 'linear', decreasing = NA, knot.pos = 0) +
+    geom_alluvium(aes(fill = signature), width = 0.001, curve_type = 'linear', decreasing = NA, knot.pos = 0, alpha = 1) +
     scale_x_discrete(breaks = seq(0, 300, 20)) +
+    scale_fill_manual('Signature', values = palette_signature)+
     labs(legend = 'Signature',
          x = "",
          y = "Exposure") +
@@ -45,26 +60,28 @@ plot_exposure_evolution <- function(sample_forest,phylo_forest,snapshot){
           plot.margin = margin(0, 0, 0, 0, "pt")) + facet_grid(type~.)
   
   
-  sign_muller <- sign_1 + muller + plot_layout(nrow = 2)
+  sign_muller <- sign_1 + muller + theme(legend.position = 'right') + plot_layout(nrow = 2)
   
-  
-  
-  
-  sign_tree  <- plot_forest(sample_forest,color_map = color_map_clones) +
+  sign_tree <- plot_forest(sample_forest,color_map = color_map_clones) + 
+    theme(legend.position = 'left') + 
+    ggplot2::guides(size = "none",
+                    shape = "none",
+                    color = ggplot2::guide_legend("Species")) +
     df_final %>%
-    group_by(time) %>%
+    group_by(time, type) %>%
     reframe(exposure = exposure/sum(exposure), across(everything())) %>%
     ggplot()+
     geom_bar(aes(x = time, y = exposure, fill = signature), stat = "identity", width = 10) +
     ylab('Exposure') +
     xlab('') +
     scale_x_reverse() +
-    #scale_fill_manual('Signature', values = signatures_palette(forest_muts,55))+
+    scale_fill_manual('Signature', values = palette_signature)+
     theme_minimal() +
     coord_flip() + theme(axis.title.y=element_blank(),
                          axis.text.y=element_blank(),
                          axis.ticks.y=element_blank(),
-                         plot.margin = margin(0, 0, 0, 0, "pt")) + facet_grid(.~type)
+                         plot.margin = margin(0, 0, 0, 0, "pt")) + facet_grid(.~type)  + 
+    plot_layout(nrow = 1, guides = 'collect')
   return(list("sign_tree"=sign_tree,"sign_muller"=sign_muller))
 }
 
--- report/plotting/utils.R
+++ report/plotting/utils.R
@@ -30,19 +30,6 @@ relative_to_absolute_coords_pos = function(x, ref = "GRCh38") {
 }
 
 
-signatures_palette <- function(phylo_forest,seed){
-  ref_path <- phylo_forest$get_reference_path()
-  SBS_table_path <- gsub(pattern = "reference.fasta",replacement = "SBS_signatures.txt",x = ref_path)
-  IDS_table_path <- gsub(pattern = "reference.fasta",replacement = "indel_signatures.txt",x = ref_path)
-  SBS_table <-  read.csv(SBS_table_path,header=T,sep="\t")
-  SBS_sign <- colnames(SBS_table)
-  IDS_table <-  read.csv(IDS_table_path,header=T,sep="\t")
-  IDS_sign <- colnames(IDS_table)
-  set.seed(seed)
-  return(Polychrome::createPalette(length(sigs), c("#6B8E23","#4169E1"), M=1000,
-                                   target="normal", range=c(15,80)) %>%
-           setNames(sigs))
-}
 #'
 #' Generates CNAqc object given simulate_seq() dataframe and
 #' phylogenetic forest object. The final result will be a CNAqc 
