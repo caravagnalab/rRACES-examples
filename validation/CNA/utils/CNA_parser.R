09859423ac7acadd0d555629c21a548312054e44
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2025-06-16 15:51:56 +0200

    add Sequenza parser for CNA validation

--- validation/CNA/utils/CNA_parser.R
+++ validation/CNA/utils/CNA_parser.R
@@ -15,3 +15,19 @@ parse_ASCAT = function(segments_file, extra_file){
     return(list(segments = segments, purity = purity, ploidy = ploidy))
 }
 
+parse_Sequenza = function(segments_file, extra_file){
+  # Extract the segments information
+  segments = readr::read_tsv(segments_file, col_types = readr::cols()) %>%
+    dplyr::rename(
+      chr = chromosome,
+      from = start.pos,
+      to = end.pos,
+      Major = A,
+      minor = B) %>%
+    dplyr::select(chr, from, to, Major, minor, dplyr::everything())
+  
+  solutions = readr::read_tsv(extra_file, col_types = readr::cols())
+  purity = solutions[["cellularity"]][2]
+  ploidy = solutions[["ploidy.estimate"]][2]
+  return(list(segments = segments, purity = purity, ploidy = ploidy))
+}
