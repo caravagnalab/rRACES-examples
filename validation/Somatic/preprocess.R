9ec0e92292794b6444902d20632c389a40ccb872
giorgiagandolfi <giorgia.gandolfi4@studio.unibo.it>
2025-06-24 23:30:26 +0200

    include file check preprocessing

--- validation/Somatic/preprocess.R
+++ validation/Somatic/preprocess.R
@@ -11,6 +11,22 @@ source("utils/strelka_utils.R")
 source("../../getters/process_getters.R")
 source("../../getters/sarek_getters.R")
 
+get_missing_files <- function(spn,purity,coverage,caller, chr,base_path="/orfeo/cephfs/scratch/cdslab/shared/SCOUT/"){
+  missing_files <- list()
+  samples <- get_sample_names(spn)
+  for (sample in samples){
+    for (type in c("SNV","INDEL")){
+      expected_file_name <- file.path(base_path,spn,"validation/somatic/",spn,paste0(coverage,"x_",purity,"p"),caller,sample,type,paste0("chr",chr,".rds"))
+      # print(expected_file_name)
+      if (!file.exists(expected_file_name)){
+        missing_files <- c(missing_files,expected_file_name)
+        # print(expected_file_name)
+      }
+    }    
+  }
+  return(missing_files)
+}
+
 option_list <- list(make_option(c("--spn_id"), type = "character", default = 'SPN01'),
                     make_option(c("--purity"), type = "character", default = '0.6'),
                     make_option(c("--coverage"), type = "character", default = '100'),
@@ -32,14 +48,31 @@ gt_path = get_mutations(spn = spn_id,
                         purity = purity, 
                         type = "tumour")
 
-message("PARSING RACES")
-process_seq_results(gt_path, spn_id, purity, coverage, chromosome, base_path = data_dir, outdir = outdir)
 
-message("PARSING MUTECT2")
-process_mutect2_results(gt_path, spn_id, purity, coverage, chromosome, base_path = data_dir, outdir = outdir)
+if (length(get_missing_files(spn_id,coverage=coverage,purity=purity,caller="process",chromosome))>0){
+  process_seq_results(gt_path, spn_id, purity, coverage, chromosome, base_path = data_dir, outdir = outdir)
+  message("PARSING ProCESS")
+} else {
+  message("ProCESS preprocessing already run!")
+}
 
-message("PARSING FREEBAYES")
-process_freebayes_results(gt_path, spn_id, purity, coverage, chromosome, base_path = data_dir, outdir = outdir, pass_quality = 20, min_vaf = 0.01, max_normal_vaf = 0.02)
+if (length(get_missing_files(spn_id,coverage=coverage,purity=purity,caller="mutect2",chromosome))>0){
+  process_mutect2_results(gt_path, spn_id, purity, coverage, chromosome, base_path = data_dir, outdir = outdir)
+  message("PARSING MUTECT2")
+} else {
+  message("MUTECT2 preprocessing already run!")
+}
 
-message("PARSING STRELKA")
-process_strelka_results(gt_path, spn_id, purity, coverage, chromosome, base_path = data_dir, outdir = outdir)
+if (length(get_missing_files(spn_id,coverage=coverage,purity=purity,caller="freebayes",chromosome))>0){
+  process_freebayes_results(gt_path, spn_id, purity, coverage, chromosome, base_path = data_dir, outdir = outdir, pass_quality = 20, min_vaf = 0.01, max_normal_vaf = 0.02)
+  message("PARSING FREEBAYES")
+} else {
+  message("FREEBAYES preprocessing already run!")
+}
+
+if (length(get_missing_files(spn_id,coverage=coverage,purity=purity,caller="strelka",chromosome))>0){
+  process_strelka_results(gt_path, spn_id, purity, coverage, chromosome, base_path = data_dir, outdir = outdir)
+  message("PARSING STRELKA")
+} else {
+  message("STRELKA preprocessing already run!")
+}
--- validation/validate_combination.py
+++ validation/validate_combination.py
@@ -5,6 +5,9 @@ import sys
 import math
 import glob
 import time
+from pathlib import Path
+import re
+
 
 import subprocess
 import argparse
@@ -65,6 +68,24 @@ if (__name__ == '__main__'):
                         help="Which step to skip, select among: cna, somatic, none")
 
     args = parser.parse_args()
+    
+    def get_sample_names(spn, base_path="/orfeo/cephfs/scratch/cdslab/shared/SCOUT/"):
+      search_path = Path(base_path) / spn / "process" / spn
+      name_files = [f.name for f in search_path.glob("*.rff")]
+      sample_names = [re.sub(r'.{4}$', '', name) for name in name_files]
+      return sample_names
+
+
+  def required_preprocessing(chr, search_path,expected_files):
+      filename="chr"+str(chr)+".rds"
+      founded_files = []
+      for root, dirs, files in os.walk(search_path):
+          if filename in files:
+              founded_files.append(os.path.join(root, filename))
+      if (len(founded_files)==expected_files):
+          return False
+      return True
+
 
     if args.account is None:
         process = subprocess.Popen(['whoami'],
@@ -88,36 +109,47 @@ if (__name__ == '__main__'):
     #curr_dir = os.getcwd()
     base_dir = args.directory
     log_dir = '{}/out/'.format(base_dir)
-
+    
+    #somatic_callers = ["mutect2","strelka",""]
 
     if "somatic" not in args.skip.split(","):
         with open('run_processing.sh', 'w') as outstream:
             outstream.write(somatic_processing_shell_script)
 
         chr_job_ids = []
+        outfile_preprocessing = outfile_preprocessing = scout_dir+"/"+args.SPN+"/validation/somatic/"+args.SPN+"/"+str(args.coverage)+"x_"+args.purity+"p"
+        n_exp =len(get_sample_names(args.SPN,scout_dir))*4*2
         
 
         for chr in chromosomes:
+            
+            if (required_preprocessing(chr, outfile_preprocessing,n_exp)):
+              
+              print("Run preprocessing for chromosome "+str(chr)")
+              
+              cmd = ['sbatch','--parsable',
+                  '--account={}'.format(account),
+                  '--partition={}'.format(args.partition),
+                  '--job-name=parsing_{}_{}_{}_{}'.format(args.SPN, args.coverage,args.purity,chr),
+                  ('--export=SPN={},COVERAGE={},PURITY={},CHROMOSOME={},DIRECTORY={}').format(args.SPN,
+                                                      args.coverage, args.purity,chr,args.directory),
+                  '--output={}/parsing_chr{}.log'.format(log_dir, chr),
+                  './run_processing.sh']
 
-            cmd = ['sbatch','--parsable',
-                '--account={}'.format(account),
-                '--partition={}'.format(args.partition),
-                '--job-name=parsing_{}_{}_{}_{}'.format(args.SPN, args.coverage,args.purity,chr),
-                ('--export=SPN={},COVERAGE={},PURITY={},CHROMOSOME={},DIRECTORY={}').format(args.SPN,
-                                                    args.coverage, args.purity,chr,args.directory),
-                '--output={}/parsing_chr{}.log'.format(log_dir, chr),
-                './run_processing.sh'] 
-
-            result = subprocess.run(cmd, stdout=subprocess.PIPE)
-            job_id = result.stdout.decode().strip()
-            chr_job_ids.append(job_id)
+              result = subprocess.run(cmd, stdout=subprocess.PIPE)
+              job_id = result.stdout.decode().strip()
+              chr_job_ids.append(job_id)
+            else:
+              print("No need to run preprocessing for chromosome "+str(chr))
         
         # Run final somatic report
 
         with open('run_reports.sh', 'w') as outstream:
-            outstream.write(somatic_prepare_report_shell_script)
+            outstream.write(somatic_prepare_report_shell_script) ## single caller
         dependency_str = ':'.join(chr_job_ids)
-        cmd = ['sbatch', '--account={}'.format(account),
+        print("Run report")
+        cmd = ['sbatch', '--parsable',
+            '--account={}'.format(account),
             '--partition={}'.format(args.partition),
             '--job-name=somatic_report_{}_{}_{}'.format(args.SPN, args.coverage,args.purity),
             '--dependency=afterok:{}'.format(dependency_str),
@@ -125,6 +157,19 @@ if (__name__ == '__main__'):
                                                 args.coverage, args.purity,args.directory),
             '--output={}/somatic_report_{}_{}_{}.log'.format(log_dir, args.SPN,args.coverage,args.purity),
             './run_reports.sh']
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        job_id = result.stdout.decode().strip()
+        
+        
+        cmd = ['sbatch', '--parsable',
+            '--account={}'.format(account),
+            '--partition={}'.format(args.partition),
+            '--job-name=somatic_report_{}_{}_{}'.format(args.SPN, args.coverage,args.purity),
+            '--dependency=afterok:{}'.format(job_id),
+            ('--export=SPN={},COVERAGE={},PURITY={},DIRECTORY={}').format(args.SPN,
+                                                args.coverage, args.purity,args.directory),
+            '--output={}/somatic_multicaller_report_{}_{}_{}.log'.format(log_dir, args.SPN,args.coverage,args.purity),
+            './run_multicaller_reports.sh']
         subprocess.run(cmd)
     
     if "cna" not in args.skip.split(","):
